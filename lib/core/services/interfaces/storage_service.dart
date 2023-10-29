ec1dfc5f5dc2f1d0fc0feaf6038b1cd7165e2593
Yurin Andrey <100288192+Namxobick@users.noreply.github.com>
2023-10-29 13:27:16 +0300

    Измененный интерфейс сервиса хранилища #15 (PR #19)

--- lib/core/services/interfaces/storage_service.dart
+++ lib/core/services/interfaces/storage_service.dart
@@ -1,5 +1,5 @@
 abstract class StorageService {
-  Future<bool> containsKey (String key, bool secure);
-  Future<String> read(String key, bool secure);
-  Future<void> write(String key, String value, bool secure);
+  Future<bool> containsKey ({required String key, bool secure = false});
+  Future<String?> read({required String key, bool secure = false});
+  Future<void> write({required String key, required String value, bool secure = false});
 }
