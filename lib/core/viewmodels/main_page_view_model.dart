81d84428fa3f6b4fcde50d138ce4ecb2e4b891d5
Mikhail Kriseev <mish.kryseev@gmail.com>
2023-11-01 00:59:32 +0300

    add: mainpage viewmodel

--- lib/core/viewmodels/main_page_view_model.dart
+++ lib/core/viewmodels/main_page_view_model.dart
@@ -1,3 +1,25 @@
 import 'package:unn_mobile/core/viewmodels/base_view_model.dart';
 
-class MainPageViewModel extends BaseViewModel {}
+class MainPageViewModel extends BaseViewModel {
+  int _selectedDrawerItem = 0;
+  int _selectedBarItem = 0;
+  bool _isDrawerItemSelected = false;
+
+  int get selectedDrawerItem => _selectedDrawerItem;
+  set selectedDrawerItem(int value) {
+    _selectedDrawerItem = value;
+    notifyListeners();
+  }
+
+  int get selectedBarItem => _selectedBarItem;
+  set selectedBarItem(value) {
+    _selectedBarItem = value;
+    notifyListeners();
+  }
+
+  bool get isDrawerItemSelected => _isDrawerItemSelected;
+  set isDrawerItemSelected(bool value) {
+    _isDrawerItemSelected = value;
+    notifyListeners();
+  }
+}
