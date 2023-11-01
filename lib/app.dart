bc785e10d42923b7790a0eccdb492acbdd41190f
Mikhail Kriseev <mish.kryseev@gmail.com>
2023-11-01 19:11:09 +0300

    Fix bug: issue #20 (#21)

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:unn_mobile/ui/views/auth_page/auth_page.dart';
+import 'package:unn_mobile/ui/router.dart' as router;
 
 class UnnMobile extends StatelessWidget {
   const UnnMobile({super.key});
@@ -8,6 +9,7 @@ class UnnMobile extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: AuthPage(),
+      onGenerateRoute: router.Router.generateRoute,
     );
   }
 }
