7bc928c15f79a5f1a0ecc22a7d85e73ac9a9c6e4
Mikhail Kriseev <mish.kryseev@gmail.com>
2023-11-02 22:28:55 +0300

    Экран с логотипом (PR #27)
    Co-authored-by: Yurin Andrey <andreyyurin@icloud.com>

---
+++ lib/core/services/implementations/authorisation_refresh_service_impl.dart
@@ -0,0 +1,25 @@
+import 'package:injector/injector.dart';
+import 'package:unn_mobile/core/services/interfaces/auth_data_provider.dart';
+import 'package:unn_mobile/core/services/interfaces/authorisation_service.dart';
+import 'package:unn_mobile/core/models/auth_data.dart';
+import 'package:unn_mobile/core/services/interfaces/authorisation_refresh_service.dart';
+
+
+class AuthorisationRefreshServiceImpl implements AuthorisationRefreshService {
+  final _authDataProvider =  Injector.appInstance.get<AuthDataProvider>();
+  final _authorisationService = Injector.appInstance.get<AuthorisationService>();
+
+  Future<bool> _userDataExistsInStorage() async{
+    AuthData authData =  await _authDataProvider.getAuthData();
+    return !(authData.login == AuthData.getDefaultParameter() || authData.login == AuthData.getDefaultParameter());
+  }
+
+  @override
+  Future<AuthRequestResult?> refreshLogin() async{
+    if (!await _userDataExistsInStorage()) {
+      return null;
+    }
+    AuthData authData =  await _authDataProvider.getAuthData();
+    return _authorisationService.auth(authData.login, authData.password);
+  }
+}
