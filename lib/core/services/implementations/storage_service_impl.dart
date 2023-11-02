f6f902f3a4bb17fd8cb56285366ecf126fd2cac4
Mikhail Kriseev <mish.kryseev@gmail.com>
2023-11-02 15:06:11 +0300

    Сервис хранилища (PR #29)

---
+++ lib/core/services/implementations/storage_service_impl.dart
@@ -0,0 +1,53 @@
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:unn_mobile/core/services/interfaces/storage_service.dart';
+
+class StorageServiceImpl extends StorageService {
+  SharedPreferences? _sharedPreferences;
+  final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
+
+  final Map<String, String?> _secureCache = {};
+  final Map<String, String?> _nonSecureCache = {};
+
+  @override
+  Future<bool> containsKey({required String key, bool secure = false}) async {
+    await _initIfNeeded();
+    var cacheMap = secure ? _secureCache : _nonSecureCache;
+    if(cacheMap.containsKey(key))
+    {
+      return true;
+    } 
+    return secure ? await _secureStorage.containsKey(key: key) : _sharedPreferences!.containsKey(key);
+  }
+
+  @override
+  Future<String?> read({required String key, bool secure = false}) async {
+    await _initIfNeeded();
+    var cacheMap = secure ? _secureCache : _nonSecureCache;
+    if(cacheMap.containsKey(key))
+    {
+      return cacheMap[key];
+    }   
+    var value = secure ? await _secureStorage.read(key: key) : _sharedPreferences!.getString(key);
+    cacheMap.putIfAbsent(key, () => value);
+    return value;
+  }
+
+  @override
+  Future<void> write({required String key, required String value, bool secure = false}) async {
+    await _initIfNeeded();
+    var cacheMap = secure ? _secureCache : _nonSecureCache;
+    cacheMap.update(key, (_) => value);
+    secure ? await _secureStorage.write(key: key, value: value) : _sharedPreferences!.setString(key, value);
+  }
+
+  Future<void> _initIfNeeded() async
+  {
+    _sharedPreferences ??= await SharedPreferences.getInstance();
+    if(_sharedPreferences == null)
+    {
+      throw Exception("Could not get storage instance");
+    }
+  }
+}
