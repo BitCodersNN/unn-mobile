0051973d1c9545ade591156caa20066adbbb341d
Mikhail Kriseev <mish.kryseev@gmail.com>
2023-11-02 15:11:43 +0300

    Добавил ключевое слово interface в интерфейсы сервисов (PR #31)

--- lib/core/services/interfaces/storage_service.dart
+++ lib/core/services/interfaces/storage_service.dart
@@ -1,4 +1,4 @@
-abstract class StorageService {
+abstract interface class StorageService {
   Future<bool> containsKey ({required String key, bool secure = false});
   Future<String?> read({required String key, bool secure = false});
   Future<void> write({required String key, required String value, bool secure = false});
