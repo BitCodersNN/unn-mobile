59f5d3f5250ff25745156eb8896ba6f7efa4a689
Mikhail Kriseev <mish.kryseev@gmail.com>
2023-11-02 14:50:52 +0300

    ViewModel рамки основного экрана (PR #25)

--- lib/core/viewmodels/main_page_view_model.dart
+++ lib/core/viewmodels/main_page_view_model.dart
@@ -1,3 +1,53 @@
+import 'package:flutter/material.dart';
 import 'package:unn_mobile/core/viewmodels/base_view_model.dart';
 
-class MainPageViewModel extends BaseViewModel {}
+class MainPageViewModel extends BaseViewModel {
+  int _selectedDrawerItem = 0;
+  int _selectedBarItem = 0;
+  bool _isDrawerItemSelected = false;
+
+  int get selectedDrawerItem => _selectedDrawerItem;
+  set selectedDrawerItem(int value) {
+    _selectedDrawerItem = value;
+    notifyListeners();
+  }
+
+  int get selectedBarItem => _selectedBarItem;
+  set selectedBarItem(value) {
+    _selectedBarItem = value;
+    notifyListeners();
+  }
+
+  bool get isDrawerItemSelected => _isDrawerItemSelected;
+  set isDrawerItemSelected(bool value) {
+    _isDrawerItemSelected = value;
+    notifyListeners();
+  }
+
+  final List<String> _barScreenNames = [
+    "Лента",
+    "Расписание",
+    "Карта",
+    "Материалы",
+  ];
+  final List<String> _drawerScreenNames = [
+    "Чаты",
+    "Справки",
+    "Сотрудники",
+    "Календарь",
+    "Подразделения",
+    "Библиотечные ресурсы",
+    "Сайт оплаты",
+  ];
+  String get selectedScreenName {
+    return isDrawerItemSelected
+        ? _drawerScreenNames[_selectedDrawerItem]
+        : _barScreenNames[_selectedBarItem];
+  }
+  List<String> get drawerScreenNames => _drawerScreenNames;
+  List<String> get barScreenNames => _barScreenNames;
+  
+  ImageProvider<Object>? get userAvatar => null;
+  String get userNameAndSurname => "Имя Фамилия";
+  String get userGroup => "3821Б1";
+}
