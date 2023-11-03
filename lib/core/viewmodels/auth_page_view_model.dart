fb6aba8c09a27c60e07383f05a8d6aafd3cdc675
Mikhail Kriseev <mish.kryseev@gmail.com>
2023-11-03 10:23:57 +0300

    Немного рефакторинга

--- lib/core/viewmodels/auth_page_view_model.dart
+++ lib/core/viewmodels/auth_page_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:injector/injector.dart';
+import 'package:unn_mobile/core/misc/auth_error_messages.dart';
 import 'package:unn_mobile/core/models/auth_data.dart';
 import 'package:unn_mobile/core/services/interfaces/auth_data_provider.dart';
 import 'package:unn_mobile/core/services/interfaces/authorisation_service.dart';
@@ -9,35 +10,24 @@ class AuthPageViewModel extends BaseViewModel {
       Injector.appInstance.get<AuthDataProvider>();
   final AuthorisationService _authorisationService =
       Injector.appInstance.get<AuthorisationService>();
-  Future<bool> login(String user, String password) async {
-    setState(ViewState.busy);
-    _resetAuthError();
-    var authResult = await _authorisationService.auth(user, password);
-    switch (authResult) {
-      case AuthRequestResult.success:
-        break;
-      case AuthRequestResult.noInternet:
-        _setAuthError('Нет подключения к Интернету');
-        break;
-      case AuthRequestResult.wrongCredentials:
-        _setAuthError('Логин или пароль неверны');
-        break;
-      case AuthRequestResult.unknownError:
-        _setAuthError('Случилась неизвестная ошибка...');
-        break;
-    }
-    if (authResult == AuthRequestResult.success) {
-      await _authDataProvider.saveAuthData(AuthData(user, password));
-    }
-    setState(ViewState.idle);
-    return authResult == AuthRequestResult.success;
-  }
 
   bool _hasAuthError = false;
   bool get hasAuthError => _hasAuthError;
 
   String _authErrorText = '';
   String get authErrorText => _authErrorText;
+
+  Future<bool> login(String user, String password) async {
+    setState(ViewState.busy);
+    _resetAuthError();
+    var authResult = await _authorisationService.auth(user, password);
+    _setAuthError(authResult.errorMessage);
+    if (authResult == AuthRequestResult.success) {
+      await _authDataProvider.saveAuthData(AuthData(user, password));
+    }
+    setState(ViewState.idle);
+    return authResult == AuthRequestResult.success;
+  }
 
   void _setAuthError(String text) {
     _hasAuthError = true;
