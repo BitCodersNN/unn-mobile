6e37e3bdbbea2ae4d9f2b1b11554f2ad3912deae
Yurin Andrey <100288192+Namxobick@users.noreply.github.com>
2023-11-03 16:53:48 +0300

    Модели и интерфейсы сервиса API расписания и связанных с ним сервисов (PR #38)

---
+++ lib/core/services/interfaces/offline_schedule_provider.dart
@@ -0,0 +1,6 @@
+import 'package:unn_mobile/core/models/subject.dart';
+
+abstract interface class OfflineScheduleProvider{
+  Future<List<Subject>> loadSchedule();
+  Future<void> saveSchedule(List<Subject> schedule);
+}
