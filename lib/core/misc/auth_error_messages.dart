8c18d19856233ef25fdd07dc08c0de5fcb251ada
Mikhail Kriseev <mish.kryseev@gmail.com>
2023-11-03 11:07:50 +0300

    AuthPageViewModel (PR #39)

---
+++ lib/core/misc/auth_error_messages.dart
@@ -0,0 +1,11 @@
+import 'package:unn_mobile/core/services/interfaces/authorisation_service.dart';
+
+extension AuthRequestResultErrorMessages on AuthRequestResult {
+  String get errorMessage => switch(this)
+  {
+    AuthRequestResult.success => '',
+    AuthRequestResult.wrongCredentials => 'Логин или пароль неверны',
+    AuthRequestResult.noInternet => 'Не получилось подключиться к серверу',
+    AuthRequestResult.unknownError => 'Случилась неизвестная ошибка',
+  };
+}
