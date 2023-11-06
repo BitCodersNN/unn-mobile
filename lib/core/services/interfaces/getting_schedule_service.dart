697bc8451c6cffee8a285670f7b6c246d294a6f3
Yurin Andrey <andreyyurin@icloud.com>
2023-11-06 22:24:30 +0300

    Добавил описание методу получения расписания

--- lib/core/services/interfaces/getting_schedule_service.dart
+++ lib/core/services/interfaces/getting_schedule_service.dart
@@ -2,5 +2,10 @@ import 'package:unn_mobile/core/models/schedule_filter.dart';
 import 'package:unn_mobile/core/models/subject.dart';
 
 abstract interface class GetScheduleService{
-  Future<List<Subject>> getSchedule(ScheduleFilter scheduleFilter);
+  /// Получение расписания
+  ///
+  /// [scheduleFilter]: Фильтр, по которому происходит получение расписания
+  ///
+  /// Возращает список предметов или 'null', если не вышло получить ответ от портала или statusCode не равен 200
+  Future<List<Subject?>> getSchedule(ScheduleFilter scheduleFilter);
 }
