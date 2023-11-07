8a98e1759c7d40508c697d17ad2eb2f68dfb8d14
Yurin Andrey <andreyyurin@icloud.com>
2023-11-07 12:11:50 +0300

    Переделал конвертор из и в Json

--- lib/core/models/subject.dart
+++ lib/core/models/subject.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-
 
 enum SubjectType {
   practice,
@@ -22,56 +20,54 @@ class Address {
   String get building => _building;
 }
 
+class KeysForSubjectJsonConverter{
+  static const String discipline = 'discipline';
+  static const String kindOfWork = 'kindOfWork';
+  static const String auditorium = 'auditorium';
+  static const String building = 'building';
+  static const String stream = 'stream';
+  static const String lecturer = 'lecturer';
+  static const String beginLesson = 'beginLesson';
+  static const String endLesson = 'endLesson';
+}
+
 class Subject {
   final String _name;
-  final SubjectType _subjectType;
+  final SubjectType _kindOfWork;
   final Address _address;
   final List<String> _groups;
   final String _lecturer;
   final DateTimeRange _dateTimeRange;
 
-  Subject(this._name, this._subjectType, this._address, this._groups,
+  Subject(this._name, this._kindOfWork, this._address, this._groups,
       this._lecturer, this._dateTimeRange);
 
   String get name => _name;
-  SubjectType get subjectType => _subjectType;
+  SubjectType get subjectType => _kindOfWork;
   Address get address => _address;
   List<String> get groups => _groups;
   String get lecturer => _lecturer;
   DateTimeRange get dateTimeRange => _dateTimeRange;
 
-  factory Subject.fromJson(Map<String, Object?> jsonMap) {
-    DateTime startDateTime = DateFormat('y.MM.dd H:m').parse('${jsonMap['date'] as String} ${jsonMap['beginLesson'] as String}');
-    DateTime endDateTime = DateFormat('y.MM.dd H:m').parse('${jsonMap['date'] as String} ${jsonMap['endLesson'] as String}');
-    
-    final subjectType = switch(jsonMap['kindOfWork'] as String) {
-      'Лекция' => SubjectType.lecture,
-      'Практика (семинарские занятия)' => SubjectType.practice,
-      'Лабораторная' => SubjectType.laboratory,
-      'Зачёт' => SubjectType.credit,
-      'Консультации перед экзаменом' => SubjectType.consultation,
-      'Экзамен' => SubjectType.exam,
-      String() => null,
-    };
-
+  factory Subject.fromJson(Map<String, Object?> jsonMap) { // переделать
     return Subject(
-      jsonMap['discipline'] as String,
-      subjectType!,
-      Address(jsonMap['auditorium'] as String, jsonMap['building'] as String),
-      (jsonMap['stream'] as String).split('|'),
-      jsonMap['lecturer'] as String,
-      DateTimeRange(start: startDateTime, end: endDateTime),
+      jsonMap[KeysForSubjectJsonConverter.discipline] as String,
+      SubjectType.values.byName(jsonMap[KeysForSubjectJsonConverter.kindOfWork] as String),
+      Address(jsonMap[KeysForSubjectJsonConverter.auditorium] as String, jsonMap[KeysForSubjectJsonConverter.building] as String),
+      (jsonMap[KeysForSubjectJsonConverter.stream] as String).split('|'),
+      jsonMap[KeysForSubjectJsonConverter.lecturer] as String,
+      DateTimeRange(start: jsonMap[KeysForSubjectJsonConverter.beginLesson] as DateTime, end: jsonMap[KeysForSubjectJsonConverter.endLesson] as DateTime),
     );
   }
 
-  Map toJson() => {
-        'discipline': _name,
-        'subjectType': _subjectType.name,
-        'building': _address.building,
-        'auditorium': _address.auditorium,
-        'groups': _groups.join('|'),
-        'lecturer': _lecturer,
-        'beginLesson': _dateTimeRange.start,
-        'endLesson': _dateTimeRange.end,
+  Map toJson() => { 
+        KeysForSubjectJsonConverter.discipline: _name,
+        KeysForSubjectJsonConverter.kindOfWork: _kindOfWork.name,
+        KeysForSubjectJsonConverter.building: _address.building,
+        KeysForSubjectJsonConverter.auditorium: _address.auditorium,
+        KeysForSubjectJsonConverter.stream: _groups.join('|'),
+        KeysForSubjectJsonConverter.lecturer: _lecturer,
+        KeysForSubjectJsonConverter.beginLesson: _dateTimeRange.start,
+        KeysForSubjectJsonConverter.endLesson: _dateTimeRange.end,
       };
 }
--- lib/core/services/implementations/get_schedule_service_impl.dart
+++ lib/core/services/implementations/get_schedule_service_impl.dart
@@ -2,7 +2,8 @@ import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 
-
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:unn_mobile/core/misc/http_helper.dart';
 import 'package:unn_mobile/core/models/schedule_filter.dart';
 import 'package:unn_mobile/core/models/subject.dart';
@@ -22,8 +23,14 @@ class GetScheduleServiceImpl implements GetScheduleService {
   Future<List<Subject>?> getSchedule(ScheduleFilter scheduleFilter) async {
     _path = '$_path${scheduleFilter.idType.name}/${scheduleFilter.id}';
     final requstSender = HttpRequestSender(path: _path, queryParams: {
-      _start: scheduleFilter.dateTimeRange.start.toIso8601String().split('T')[0].replaceAll('-', '.'),
-      _finish: scheduleFilter.dateTimeRange.end.toIso8601String().split('T')[0].replaceAll('-', '.'),
+      _start: scheduleFilter.dateTimeRange.start
+          .toIso8601String()
+          .split('T')[0]
+          .replaceAll('-', '.'),
+      _finish: scheduleFilter.dateTimeRange.end
+          .toIso8601String()
+          .split('T')[0]
+          .replaceAll('-', '.'),
       _lng: '1',
     });
 
@@ -40,15 +47,42 @@ class GetScheduleServiceImpl implements GetScheduleService {
     if (_statusCode != 200) {
       return null;
     }
-    
+
     List<dynamic> jsonList = jsonDecode(await responseToStringBody(response));
-    
+
     List<Subject> schedule = [];
-    
+
     for (var jsonMap in jsonList) {
-      schedule.add(Subject.fromJson(jsonMap));
+      schedule.add(_convertFromJson(jsonMap));
     }
 
     return schedule;
+  }
+
+  Subject _convertFromJson(Map<String, Object?> jsonMap) {
+    DateTime startDateTime = DateFormat('y.MM.dd H:m').parse(
+        '${jsonMap['date'] as String} ${jsonMap[KeysForSubjectJsonConverter.beginLesson] as String}');
+    DateTime endDateTime = DateFormat('y.MM.dd H:m').parse(
+        '${jsonMap['date'] as String} ${jsonMap[KeysForSubjectJsonConverter.endLesson] as String}');
+
+    final subjectType = switch (jsonMap[KeysForSubjectJsonConverter.kindOfWork] as String) {
+      'Лекция' => SubjectType.lecture,
+      'Практика (семинарские занятия)' => SubjectType.practice,
+      'Лабораторная' => SubjectType.laboratory,
+      'Зачёт' => SubjectType.credit,
+      'Консультации перед экзаменом' => SubjectType.consultation,
+      'Экзамен' => SubjectType.exam,
+      String() => null,
+    };
+
+    return Subject(
+      jsonMap[KeysForSubjectJsonConverter.discipline] as String,
+      subjectType!,
+      Address(jsonMap[KeysForSubjectJsonConverter.auditorium] as String,
+          jsonMap[KeysForSubjectJsonConverter.building] as String),
+      (jsonMap[KeysForSubjectJsonConverter.stream] as String).split('|'),
+      jsonMap[KeysForSubjectJsonConverter.lecturer] as String,
+      DateTimeRange(start: startDateTime, end: endDateTime),
+    );
   }
 }
