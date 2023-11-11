2fb696712225e3d5a161dcb8beb8dadf3494344f
Yurin Andrey <100288192+Namxobick@users.noreply.github.com>
2023-11-12 01:23:53 +0300

    Описания к методам интерфейсов (PR #45)
    Co-authored-by: Mikhail Kriseev <mish.kryseev@gmail.com>

--- lib/core/services/interfaces/offline_schedule_provider.dart
+++ lib/core/services/interfaces/offline_schedule_provider.dart
@@ -1,6 +1,12 @@
 import 'package:unn_mobile/core/models/subject.dart';
 
 abstract interface class OfflineScheduleProvider{
+  /// Загрузка расписания из хранилища
+  /// 
+  /// Возращает список предметов или 'null', если нет сохранённого расписания в хранилище
   Future<List<Subject>?> loadSchedule();
+  /// Сохраняет расписание в хранилище. Если расписание уже сохранено в хранилище, то старое удаляется и записывается новое
+  /// 
+  /// [schedule]: Список предметов
   Future<void> saveSchedule(List<Subject> schedule);
 }
