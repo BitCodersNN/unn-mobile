6df548771febf49feafdd47539ba1bb125df6921
Mikhail Kriseev <mish.kryseev@gmail.com>
2023-11-23 17:27:16 +0300

    Необходимые строчки для темы

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:unn_mobile/ui/router.dart' as router;
+import 'package:unn_mobile/ui/unn_mobile_colors.dart';
 import 'package:unn_mobile/ui/views/loading_page/loading_page.dart';
 
 class UnnMobile extends StatelessWidget {
@@ -12,7 +13,14 @@ class UnnMobile extends StatelessWidget {
       onGenerateRoute: router.Router.generateRoute,
       theme: ThemeData(
         useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xAA1A63B7)),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xAA1A63B7),
+          background: const Color(0xFFF9FAFF),
+          surface: const Color(0xFFFFFFFF),
+          surfaceVariant: const Color(0xFFEFF1FB),
+        ),
+        textTheme: Typography.blackRedwoodCity,
+        extensions: const [UnnMobileColors(scheduleDayHighlight: Color(0xFFEEEEEE), scheduleSubjectHighlight: Color(0xFFFFF6E8))]
       ),
     );
   }
