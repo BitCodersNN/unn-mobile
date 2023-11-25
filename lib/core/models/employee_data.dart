4bdadcfd23b8d79809fe646fe14b14ad36817e05
Yurin Andrey <100288192+Namxobick@users.noreply.github.com>
2023-11-25 19:43:44 +0300

    Модели для сервиса получения информации о пользователе (PR #80)

---
+++ lib/core/models/employee_data.dart
@@ -0,0 +1,68 @@
+import 'package:unn_mobile/core/models/user_data.dart';
+
+class KeysForEmployeeDataJsonConverter {
+    static const String syncID = 'sync_id';
+    static const String jobType = 'job_type';
+    static const String jobTitle = 'job_title';
+    static const String dapartment = 'dapartment';
+    static const String child = 'child';
+    static const String title = 'title';
+    static const String manager = 'manager';
+    static const String fullname = 'fullname';
+}
+
+class EmployeeData extends UserData {
+  final String _syncID;
+  final String _jobType;
+  final String _jobTitle;
+  final String _department;
+  final String _manager;
+
+  EmployeeData(
+    UserData userData,
+    this._syncID,
+    this._jobType,
+    this._jobTitle,
+    this._department,
+    this._manager,
+    ): super(userData.login, userData.fullname, userData.email, userData.phone, userData.sex, userData.urlPhoto);
+
+    String get syncID => _syncID;
+    String get jobType => _jobType;
+    String get jobTitel => _jobTitle;
+    String get department => _department;
+    String get manager => _manager;
+
+    factory EmployeeData.fromJson(Map<String, Object?> jsonMap) {
+      return EmployeeData(
+        UserData.fromJson(jsonMap),
+        jsonMap[KeysForEmployeeDataJsonConverter.syncID] as String,
+        jsonMap[KeysForEmployeeDataJsonConverter.jobType] as String,
+        jsonMap[KeysForEmployeeDataJsonConverter.jobTitle] as String,
+        ((((jsonMap[KeysForEmployeeDataJsonConverter.dapartment]
+         as Map<String, Object?>)[KeysForEmployeeDataJsonConverter.child]
+          as Map<String, Object?>)[KeysForEmployeeDataJsonConverter.child])
+           as Map<String, Object?>)[KeysForEmployeeDataJsonConverter.title] as String,
+        (jsonMap[KeysForEmployeeDataJsonConverter.manager]
+         as Map<String, Object?>)[KeysForEmployeeDataJsonConverter.fullname] as String
+      );
+    }
+
+    @override
+    Map<String, dynamic> toJson() => {
+      ... super.toJson(),
+      KeysForEmployeeDataJsonConverter.syncID: _syncID,
+      KeysForEmployeeDataJsonConverter.jobType: _jobType,
+      KeysForEmployeeDataJsonConverter.jobTitle: _jobTitle,
+      KeysForEmployeeDataJsonConverter.dapartment: {
+        KeysForEmployeeDataJsonConverter.child: {
+          KeysForEmployeeDataJsonConverter.child: {
+            KeysForEmployeeDataJsonConverter.title: _department,
+          },
+        },
+      },
+      KeysForEmployeeDataJsonConverter.manager: {
+        KeysForEmployeeDataJsonConverter.fullname: _manager,
+      },
+    };
+}
---
+++ lib/core/models/student_data.dart
@@ -0,0 +1,86 @@
+import 'package:unn_mobile/core/models/user_data.dart';
+
+class KeysForStudentDataJsonConverter {
+  static const String eduForm = 'edu_form';
+  static const String eduStatus = 'edu_status';
+  static const String eduCourse = 'edu_course';
+  static const String eduYear = 'edu_year';
+  static const String eduLevel = 'edu_level';
+  static const String faculty = 'faculty';
+  static const String eduDirection = 'edu_direction';
+  static const String eduGroup = 'edu_group';
+  static const String eduSpecialization = 'edu_specialization';
+  static const String title = 'title';
+}
+
+class StudentData extends UserData {
+  final String _eduForm;
+  final String _eduStatus;
+  final int _eduCourse;
+  final int _eduYear;
+  final String _eduLevel;
+  final String _faculty;
+  final String _eduDirection;
+  final String _eduGroup;
+  final String _eduSpecialization;
+
+  StudentData(
+    UserData userData,
+    this._eduForm,
+    this._eduStatus,
+    this._eduCourse,
+    this._eduYear,
+    this._eduLevel, 
+    this._faculty, 
+    this._eduDirection, 
+    this._eduGroup, 
+    this._eduSpecialization
+    ):  super(userData.login, userData.fullname, userData.email, userData.phone, userData.sex, userData.urlPhoto);
+
+    String get eduForm => _eduForm;
+    String get eduStatus => _eduStatus;
+    int get eduCourse => _eduCourse;
+    int get eduYear => _eduYear;
+    String get eduLevel => _eduLevel;
+    String get faculty => _faculty;
+    String get eduDirection => _eduDirection;
+    String get eduGroup => _eduGroup;
+    String get eduSpecialization => _eduSpecialization;
+
+    factory StudentData.fromJson(Map<String, Object?> jsonMap) {
+      return StudentData(
+        UserData.fromJson(jsonMap),
+        jsonMap[KeysForStudentDataJsonConverter.eduForm] as String,
+        jsonMap[KeysForStudentDataJsonConverter.eduStatus] as String,
+        jsonMap[KeysForStudentDataJsonConverter.eduCourse] as int,
+        jsonMap[KeysForStudentDataJsonConverter.eduYear] as int,
+        jsonMap[KeysForStudentDataJsonConverter.eduLevel] as String,
+        (jsonMap[KeysForStudentDataJsonConverter.faculty] as Map<String, Object?>)[KeysForStudentDataJsonConverter.title] as String,
+        (jsonMap[KeysForStudentDataJsonConverter.eduDirection] as Map<String, Object?>)[KeysForStudentDataJsonConverter.title] as String,
+        (jsonMap[KeysForStudentDataJsonConverter.eduGroup] as Map<String, Object?>)[KeysForStudentDataJsonConverter.title] as String,
+        (jsonMap[KeysForStudentDataJsonConverter.eduSpecialization] as Map<String, Object?>)[KeysForStudentDataJsonConverter.title] as String,
+      );
+    }
+
+    @override
+    Map<String, dynamic> toJson() => {
+    ... super.toJson(),
+    KeysForStudentDataJsonConverter.eduForm: _eduForm,
+    KeysForStudentDataJsonConverter.eduStatus: _eduStatus,
+    KeysForStudentDataJsonConverter.eduCourse: _eduCourse,
+    KeysForStudentDataJsonConverter.eduYear: _eduYear,
+    KeysForStudentDataJsonConverter.eduLevel: _eduLevel,
+    KeysForStudentDataJsonConverter.faculty: {
+      KeysForStudentDataJsonConverter.title: _faculty,
+    },
+    KeysForStudentDataJsonConverter.eduDirection: {
+      KeysForStudentDataJsonConverter.title: _eduDirection,
+    },
+    KeysForStudentDataJsonConverter.eduGroup: {
+      KeysForStudentDataJsonConverter.title: _eduGroup,
+    },
+    KeysForStudentDataJsonConverter.eduSpecialization: {
+      KeysForStudentDataJsonConverter.title: _eduSpecialization,
+    },
+  };
+}
