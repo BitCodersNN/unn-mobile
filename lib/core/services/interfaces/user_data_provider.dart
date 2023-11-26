5bc47cf0370f56b52d23d821e74971af0f04cd1e
Yurin Andrey <andreyyurin@icloud.com>
2023-11-26 16:05:32 +0300

    Сохранение в хранилище и получение из него профиля

---
+++ lib/core/services/interfaces/user_data_provider.dart
@@ -0,0 +1,10 @@
+import 'package:unn_mobile/core/models/user_data.dart';
+
+abstract interface class UserDataProvider {
+  /// Получает данные профиля пользователя из хранилища
+  /// 
+  /// Возращает объект [UserData] или null, если в хранилище нет сохранённого профиля
+  Future<UserData?> getUserData();
+  // Сохраняет данные профиля пользователя
+  Future<void> saveUserData(UserData userData);
+}
