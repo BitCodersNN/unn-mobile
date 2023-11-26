5545ff98c56c6b87da9546c78d4a7270dee9d7ce
Yurin Andrey <100288192+Namxobick@users.noreply.github.com>
2023-11-26 11:21:13 +0300

    Интерфейс и реализация сервиса получения профиля текущего пользователя (PR #87)

---
+++ lib/core/services/implementations/getting_profile_of_current_user_service_impl.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:injector/injector.dart';
+import 'package:unn_mobile/core/misc/http_helper.dart';
+import 'package:unn_mobile/core/models/employee_data.dart';
+import 'package:unn_mobile/core/models/student_data.dart';
+import 'package:unn_mobile/core/models/user_data.dart';
+import 'package:unn_mobile/core/services/interfaces/authorisation_service.dart';
+import 'package:unn_mobile/core/services/interfaces/getting_profile_of_current_user_service.dart';
+
+class GettingProfileOfCurrentUserImpl implements GettingProfileOfCurrentUser {
+  final String _path = 'bitrix/vuz/api/profile/current';
+  final String _sessionIdCookieKey = "PHPSESSID";
+
+  
+  @override
+  Future<UserData?> getProfileOfCurrentUser() async {
+    final authorisationService = Injector.appInstance.get<AuthorisationService>();
+
+    final requstSender = HttpRequestSender(path: _path, cookies: {
+      _sessionIdCookieKey: authorisationService.sessionId as String,
+    });
+    
+    HttpClientResponse response;
+    try {
+      response = await requstSender.get();
+    } catch (e) {
+      log(e.toString());
+      return null;
+    }
+
+    final statusCode = response.statusCode;
+
+    if (statusCode != 200) {
+      return null;
+    }
+
+    final jsonMap = jsonDecode(await HttpRequestSender.responseToStringBody(response));
+
+    return jsonMap['type'] == 'student' ? StudentData.fromJson(jsonMap) : 
+      jsonMap['type'] == 'employee' ? EmployeeData.fromJson(jsonMap) : null;
+  }
+
+}
