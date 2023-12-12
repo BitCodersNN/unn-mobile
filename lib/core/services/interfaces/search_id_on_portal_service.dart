2f309fa285cec410dab88647a717edf19fd553af
Yurin Andrey <100288192+Namxobick@users.noreply.github.com>
2023-12-12 19:32:56 +0300

    Исправил орфографические ошибки в описании методов (PR #158)

--- lib/core/services/interfaces/search_id_on_portal_service.dart
+++ lib/core/services/interfaces/search_id_on_portal_service.dart
@@ -4,13 +4,13 @@ import 'package:unn_mobile/core/models/schedule_filter.dart';
 abstract interface class SearchIdOnPortalService {
   /// Получение ID текущего пользователя, которое используется на unn-portal
   /// 
-  /// Возращает [IDForSchedule] или 'null', если не вышло получить ответ от портала или statusCode не равен 200
+  /// Возвращает [IDForSchedule] или 'null', если не вышло получить ответ от портала или statusCode не равен 200
   Future<IDForSchedule?> getIdOfLoggedInUser();
   /// Получения ID, которое используется на unn-portal
   ///
   /// [value]: Значение, по которому ищется ID
   /// [valueType]: Тип значения: группа или ФИО студента, или ФИО преподователя, или аудитория
   /// 
-  /// Возращает [ScheduleSearchResultItem] или 'null', если не вышло получить ответ от портала или statusCode не равен 200
+  /// Возвращает [ScheduleSearchResultItem] или 'null', если не вышло получить ответ от портала или statusCode не равен 200
   Future<List<ScheduleSearchResultItem>?> findIDOnPortal(String value, IDType valueType);
 }
--- lib/core/services/interfaces/user_data_provider.dart
+++ lib/core/services/interfaces/user_data_provider.dart
@@ -3,7 +3,7 @@ import 'package:unn_mobile/core/models/user_data.dart';
 abstract interface class UserDataProvider {
   /// Получает данные профиля пользователя из хранилища
   /// 
-  /// Возращает объект [UserData] или null, если в хранилище нет сохранённого профиля
+  /// Возвращает объект [UserData] или null, если в хранилище нет сохранённого профиля
   Future<UserData?> getUserData();
   // Сохраняет данные профиля пользователя
   Future<void> saveUserData(UserData userData);
