68ca8c9f898769b684e9a4c04e6f86f3ffa1cad8
Yurin Andrey <100288192+Namxobick@users.noreply.github.com>
2023-12-26 21:54:47 +0300

    Перенеc инициализацию профиля пользователя в VM загрузочного экрана и исправил, выявленные баги (#169)

--- lib/core/misc/type_of_current_user.dart
+++ lib/core/misc/type_of_current_user.dart
@@ -1,22 +1,27 @@
 import 'package:injector/injector.dart';
+import 'package:unn_mobile/core/models/student_data.dart';
 import 'package:unn_mobile/core/models/user_data.dart';
 import 'package:unn_mobile/core/services/interfaces/getting_profile_of_current_user_service.dart';
 import 'package:unn_mobile/core/services/interfaces/user_data_provider.dart';
 
-class TypeOfCurrenUser {
+class TypeOfCurrentUser {
   final UserDataProvider _userDataProvider =
       Injector.appInstance.get<UserDataProvider>();
   final GettingProfileOfCurrentUser _gettingProfileOfCurrentUser =
       Injector.appInstance.get<GettingProfileOfCurrentUser>();
-  Type? _typeOfUser;
+
+  Type _typeOfUser = StudentData;
+  Type get typeOfUser => _typeOfUser;
 
   /// Возвращает тип текущего пользователя: [Type] ([StudentData] или [EmployeeData])
   Future<Type> getTypeOfCurrentUser() async {
-    if (_typeOfUser == null) {
-      UserData? type = await _userDataProvider.getUserData() ??
-          await _gettingProfileOfCurrentUser.getProfileOfCurrentUser();
-      _typeOfUser = type.runtimeType;
-    }
-    return _typeOfUser!;
+    UserData? type = await _userDataProvider.getUserData() ??
+        await _gettingProfileOfCurrentUser.getProfileOfCurrentUser();
+    return type.runtimeType;
+  }
+
+  /// Обновляет тип текущего пользователя
+  Future<void> updateTypeOfCurrentUser() async {
+    _typeOfUser = await getTypeOfCurrentUser();
   }
 }
--- lib/core/viewmodels/loading_page_view_model.dart
+++ lib/core/viewmodels/loading_page_view_model.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:injector/injector.dart';
+import 'package:unn_mobile/core/misc/type_of_current_user.dart';
 import 'package:unn_mobile/core/services/interfaces/authorisation_service.dart';
 import 'package:unn_mobile/core/services/interfaces/authorisation_refresh_service.dart';
+import 'package:unn_mobile/core/services/interfaces/getting_profile_of_current_user_service.dart';
+import 'package:unn_mobile/core/services/interfaces/user_data_provider.dart';
 import 'package:unn_mobile/core/viewmodels/base_view_model.dart';
 import 'package:unn_mobile/ui/router.dart';
 
@@ -13,19 +16,32 @@ enum _TypeScreen {
 class LoadingPageViewModel extends BaseViewModel {
   final _initializingApplicationService =
       Injector.appInstance.get<AuthorisationRefreshService>();
+  final TypeOfCurrentUser _typeOfCurrnetUser =
+      Injector.appInstance.get<TypeOfCurrentUser>();
+  final GettingProfileOfCurrentUser _gettingProfileOfCurrentUser =
+      Injector.appInstance.get<GettingProfileOfCurrentUser>();
+  final UserDataProvider _userDataProvider = 
+      Injector.appInstance.get<UserDataProvider>();
 
   void disateRoute(context) {
     _init().then((value) => _goToScreen(context, value));
   }
 
-  Future<_TypeScreen> _init() async =>
-      switch (await _initializingApplicationService.refreshLogin()) {
-        null => _TypeScreen.authScreen,
-        AuthRequestResult.success => _TypeScreen.mainScreen,
-        AuthRequestResult.noInternet => _TypeScreen.mainScreen,
-        AuthRequestResult.wrongCredentials => _TypeScreen.authScreen,
-        AuthRequestResult.unknownError => throw Exception('Unknown Error'),
-      };
+  Future<_TypeScreen> _init() async {
+    _TypeScreen typeScreen = switch (await _initializingApplicationService.refreshLogin()) {
+      null => _TypeScreen.authScreen,
+      AuthRequestResult.success => _TypeScreen.mainScreen,
+      AuthRequestResult.noInternet => _TypeScreen.mainScreen,
+      AuthRequestResult.wrongCredentials => _TypeScreen.authScreen,
+      AuthRequestResult.unknownError => throw Exception('Unknown Error'),
+    };
+
+    if (typeScreen == _TypeScreen.mainScreen) {
+      await _initUserData();
+    }
+
+    return typeScreen;
+  }
 
   void _goToScreen(context, _TypeScreen typeScreen) {
     final routes = switch (typeScreen) {
@@ -33,5 +49,13 @@ class LoadingPageViewModel extends BaseViewModel {
       _TypeScreen.mainScreen => Routes.mainPagePrefix,
     };
     Navigator.of(context!).pushNamedAndRemoveUntil(routes, (route) => false);
+  }
+
+  Future<void> _initUserData() async {
+    final profile = await _gettingProfileOfCurrentUser.getProfileOfCurrentUser();
+    if (profile != null) {
+      _userDataProvider.saveUserData((await _gettingProfileOfCurrentUser.getProfileOfCurrentUser())!);
+    }
+    await _typeOfCurrnetUser.updateTypeOfCurrentUser(); 
   }
 }
