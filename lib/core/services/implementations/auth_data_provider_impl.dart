e7f3c3e4bf32e5e60e7a8ecd2b7f5ed2e849fbb2
Yurin Andrey <100288192+Namxobick@users.noreply.github.com>
2023-12-31 00:02:25 +0300

    Реализовал общий интерфейс для providers и добавил метод для проверки наличия содержимого (#194)

--- lib/core/services/implementations/auth_data_provider_impl.dart
+++ lib/core/services/implementations/auth_data_provider_impl.dart
@@ -3,26 +3,43 @@ import 'package:unn_mobile/core/services/interfaces/auth_data_provider.dart';
 import 'package:unn_mobile/core/services/interfaces/storage_service.dart';
 import 'package:unn_mobile/core/models/auth_data.dart';
 
-
-class _AuthDataProviderKeys{
+class _AuthDataProviderKeys {
   static const _loginKey = 'login_key';
   static const _passwotdKey = 'password_key';
 }
 
-class AuthDataProviderImpl implements AuthDataProvider{
+class AuthDataProviderImpl implements AuthDataProvider {
   final _securityStorage = Injector.appInstance.get<StorageService>();
 
   @override
-  Future<AuthData> getAuthData() async {
-    var login = await _securityStorage.read(key: _AuthDataProviderKeys._loginKey, secure: true) ?? AuthData.getDefaultParameter();
-    var password = await _securityStorage.read(key: _AuthDataProviderKeys._passwotdKey, secure: true) ?? AuthData.getDefaultParameter();
+  Future<AuthData> getData() async {
+    var login = await _securityStorage.read(
+            key: _AuthDataProviderKeys._loginKey, secure: true) ??
+        AuthData.getDefaultParameter();
+    var password = await _securityStorage.read(
+            key: _AuthDataProviderKeys._passwotdKey, secure: true) ??
+        AuthData.getDefaultParameter();
 
     return AuthData(login, password);
   }
 
   @override
-  Future<void> saveAuthData(AuthData authData) async{
-    await _securityStorage.write(key: _AuthDataProviderKeys._loginKey, value: authData.login, secure: true);
-    await _securityStorage.write(key: _AuthDataProviderKeys._passwotdKey, value: authData.password, secure: true);
+  Future<void> saveData(AuthData authData) async {
+    await _securityStorage.write(
+        key: _AuthDataProviderKeys._loginKey,
+        value: authData.login,
+        secure: true);
+    await _securityStorage.write(
+        key: _AuthDataProviderKeys._passwotdKey,
+        value: authData.password,
+        secure: true);
+  }
+
+  @override
+  Future<bool> isContained() async {
+    return (await _securityStorage.containsKey(
+            key: _AuthDataProviderKeys._loginKey) &&
+        await _securityStorage.containsKey(
+            key: _AuthDataProviderKeys._passwotdKey));
   }
 }
