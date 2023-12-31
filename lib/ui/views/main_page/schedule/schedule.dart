b8c95a67a81710925dcd486ec9f2b2a1c8b91877
Yurin Andrey <100288192+Namxobick@users.noreply.github.com>
2023-12-31 15:22:05 +0300

    Добавил диалоговое окно при изменении onlineStatus (#201)
    Co-authored-by: Mikhail Kriseev <mish.kryseev@gmail.com>

--- lib/ui/views/main_page/schedule/schedule.dart
+++ lib/ui/views/main_page/schedule/schedule.dart
@@ -3,6 +3,7 @@ import 'package:injector/injector.dart';
 import 'package:unn_mobile/core/misc/type_of_current_user.dart';
 import 'package:unn_mobile/core/models/employee_data.dart';
 import 'package:unn_mobile/core/models/schedule_filter.dart';
+import 'package:unn_mobile/ui/builders/online_status_stream_builder.dart';
 import 'package:unn_mobile/ui/views/main_page/main_page_tab_state.dart';
 import 'package:unn_mobile/ui/views/main_page/schedule/widgets/schedule_tab.dart';
 
@@ -37,30 +38,45 @@ class _ScheduleScreenViewState extends State<ScheduleScreenView>
     final tabTexts = _getTabTexts(typeOfCurrnetUser.typeOfUser);
     final idTypesForSchedulTab =
         _getIDTypesForSchedulTab(typeOfCurrnetUser.typeOfUser);
-    return Column(
-      children: [
-        TabBar(
-          indicatorSize: TabBarIndicatorSize.label,
-          tabAlignment: TabAlignment.center,
-          isScrollable: true,
-          tabs: [
-            for (var i = 0; i < tabKeys.length; i++)
-              Tab(
-                text: tabTexts[i],
-              ),
-          ],
-          controller: _tabController,
-        ),
-        Expanded(
-          child: TabBarView(
-            controller: _tabController,
-            children: [
-              for (int i = 0; i < idTypesForSchedulTab.length; i++)
-                ScheduleTab(idTypesForSchedulTab[i], key: tabKeys[i]),
+
+    final expanded = _createExpanded(idTypesForSchedulTab);
+
+    return OnlineStatusStreamBuilder(
+      onlineWidget: Column(
+        children: [
+          TabBar(
+            indicatorSize: TabBarIndicatorSize.label,
+            tabAlignment: TabAlignment.center,
+            isScrollable: true,
+            tabs: [
+              for (var i = 0; i < tabKeys.length; i++)
+                Tab(
+                  text: tabTexts[i],
+                ),
             ],
+            controller: _tabController,
           ),
-        ),
-      ],
+          expanded,
+        ],
+      ),
+      offlineWidget: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: Text(
+              tabTexts[0],
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.black,
+                fontFamily: "Inter",
+                fontSize: 17,
+                decorationColor: Colors.blue,
+              ),
+            ),
+          ),
+          expanded,
+        ],
+      ),
     );
   }
 
@@ -76,6 +92,18 @@ class _ScheduleScreenViewState extends State<ScheduleScreenView>
       return [IDType.person, IDType.student, IDType.group];
     }
     return [IDType.student, IDType.group, IDType.person];
+  }
+
+  Expanded _createExpanded(idTypesForSchedulTab) {
+    return Expanded(
+      child: TabBarView(
+        controller: _tabController,
+        children: [
+          for (int i = 0; i < idTypesForSchedulTab.length; i++)
+            ScheduleTab(idTypesForSchedulTab[i], key: tabKeys[i]),
+        ],
+      ),
+    );
   }
 
   @override
