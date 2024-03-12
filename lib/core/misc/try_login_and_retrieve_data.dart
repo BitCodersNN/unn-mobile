fed07b574c50606973a5f2f91ebe1afa35fd3ad2
Yurin Andrey <andreyyurin@icloud.com>
2024-03-12 22:10:52 +0300

    Добавил обработку исключения

--- lib/core/misc/try_login_and_retrieve_data.dart
+++ lib/core/misc/try_login_and_retrieve_data.dart
@@ -1,18 +1,28 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:injector/injector.dart';
 import 'package:unn_mobile/core/models/online_status_data.dart';
 import 'package:unn_mobile/core/services/interfaces/authorisation_refresh_service.dart';
 import 'package:unn_mobile/core/services/interfaces/authorisation_service.dart';
 
 /// Получает данные или от функции, требующей доступ к серверу, или от функции, использующей локальное хранилище
-/// 
+///
 /// Возвращает данные полученные от [online], если есть интернет и сервер работает, иначе возвращает [offline]
-Future<T?> tryLoginAndRetrieveData<T>(Function online, Function offline) async{
-  final AuthorisationService authorisationService = Injector.appInstance.get<AuthorisationService>();
-  final AuthorisationRefreshService authorisationRefreshService = Injector.appInstance.get<AuthorisationRefreshService>();
-  final OnlineStatusData onlineStatus = Injector.appInstance.get<OnlineStatusData>();
-    
+Future<T?> tryLoginAndRetrieveData<T>(Function online, Function offline) async {
+  final AuthorisationService authorisationService =
+      Injector.appInstance.get<AuthorisationService>();
+  final AuthorisationRefreshService authorisationRefreshService =
+      Injector.appInstance.get<AuthorisationRefreshService>();
+  final OnlineStatusData onlineStatus =
+      Injector.appInstance.get<OnlineStatusData>();
+
   if (authorisationService.sessionId == '' || !onlineStatus.isOnline) {
-    final authRequestResult = await authorisationRefreshService.refreshLogin();
+    AuthRequestResult? authRequestResult;
+    try {
+      authRequestResult = await authorisationRefreshService.refreshLogin();
+    } catch (e, stacktrace) {
+      await FirebaseCrashlytics.instance
+          .log("Exception: $e\nStackTrace: \n$stacktrace");
+    }
 
     if (authRequestResult != AuthRequestResult.success) {
       return await offline();
@@ -23,8 +33,7 @@ Future<T?> tryLoginAndRetrieveData<T>(Function online, Function offline) async{
   if (result != null) {
     onlineStatus.isOnline = true;
     onlineStatus.timeOfLastOnline = DateTime.now();
-  }
-  else {
+  } else {
     onlineStatus.isOnline = false;
     result = await offline();
   }
