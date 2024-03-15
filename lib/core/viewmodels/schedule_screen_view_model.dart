3643893cc5d560f73f481a407c5ccdc8afdc280a
Yurin Andrey <andreyyurin@icloud.com>
2024-03-15 18:17:53 +0300

    По группам теперь берётся тоже следующая неделя
    Также цифры заменил на константы

--- lib/core/viewmodels/schedule_screen_view_model.dart
+++ lib/core/viewmodels/schedule_screen_view_model.dart
@@ -47,7 +47,8 @@ class ScheduleScreenViewModel extends BaseViewModel {
   Future<Map<int, List<Subject>>>? _scheduleLoader;
   Future<Map<int, List<Subject>>>? get scheduleLoader => _scheduleLoader;
   int displayedWeekOffset = 0;
-  DateTimeRange get displayedWeek => offline ?  DateTimeRanges.currentWeek() : _filter.dateTimeRange;
+  DateTimeRange get displayedWeek =>
+      offline ? DateTimeRanges.currentWeek() : _filter.dateTimeRange;
   ScheduleFilter _filter =
       ScheduleFilter(IDType.student, '', DateTimeRanges.currentWeek());
   String _searchPlaceholderText = '';
@@ -56,43 +57,46 @@ class ScheduleScreenViewModel extends BaseViewModel {
 
   void Function(Map<int, List<Subject>>)? _onScheduleLoaded;
 
+  DateTimeRange decidePivotWeek() => DateTime.now().weekday == DateTime.sunday
+      ? DateTimeRanges.nextWeek()
+      : DateTimeRanges.currentWeek();
+
   void _initHuman(String placeholderText, IDType idType) {
     _searchPlaceholderText = placeholderText;
-    tryLoginAndRetrieveData(_searchIdOnPortalService.getIdOfLoggedInUser, () => null).then(
-      (value) async {
-        if (value == null) {
-          _updateScheduleLoader();
-          return;
-        }
-        if (value.idType == idType) {
-          _filter = ScheduleFilter(
-              value.idType, value.id, decidePivotWeek());
-          _currentId = value.id;
-          _updateScheduleLoader();
-        }
-        notifyListeners();
+    tryLoginAndRetrieveData(
+            _searchIdOnPortalService.getIdOfLoggedInUser, () => null)
+        .then((value) async {
+      if (value == null) {
+        _updateScheduleLoader();
+        return;
+      }
+      if (value.idType == idType) {
+        _filter = ScheduleFilter(value.idType, value.id, decidePivotWeek());
+        _currentId = value.id;
+        _updateScheduleLoader();
+      }
+      notifyListeners();
     });
   }
 
-  DateTimeRange decidePivotWeek() => DateTime.now().weekday == 7 ? DateTimeRanges.nextWeek() : DateTimeRanges.currentWeek();
-
   void _initGroup() {
     _searchPlaceholderText = _groupNameText;
-    tryLoginAndRetrieveData(_gettingProfileOfCurrentUser.getProfileOfCurrentUser, () => null).then(
-      (value) async {
-        if (value == null) {
-          _updateScheduleLoader();
-          return;
-        }
-        if (value is StudentData) {
-          final groupID = await _searchIdOnPortalService.findIDOnPortal(
-              value.eduGroup, IDType.group);
-          _filter = ScheduleFilter(IDType.group, groupID!.first.id,
-              DateTimeRanges.currentWeek());
-          _currentId = groupID.first.id;
-          _updateScheduleLoader();
-        }
-        notifyListeners();
+    tryLoginAndRetrieveData(
+            _gettingProfileOfCurrentUser.getProfileOfCurrentUser, () => null)
+        .then((value) async {
+      if (value == null) {
+        _updateScheduleLoader();
+        return;
+      }
+      if (value is StudentData) {
+        final groupID = await _searchIdOnPortalService.findIDOnPortal(
+            value.eduGroup, IDType.group);
+        _filter =
+            ScheduleFilter(IDType.group, groupID!.first.id, decidePivotWeek());
+        _currentId = groupID.first.id;
+        _updateScheduleLoader();
+      }
+      notifyListeners();
     });
   }
 
@@ -102,8 +106,10 @@ class ScheduleScreenViewModel extends BaseViewModel {
       _filter.idType,
       _filter.id,
       DateTimeRange(
-        start: _filter.dateTimeRange.start.add(const Duration(days: 7)),
-        end: _filter.dateTimeRange.end.add(const Duration(days: 7)),
+        start: _filter.dateTimeRange.start
+            .add(const Duration(days: DateTime.daysPerWeek)),
+        end: _filter.dateTimeRange.end
+            .add(const Duration(days: DateTime.daysPerWeek)),
       ),
     );
     _updateScheduleLoader();
@@ -116,8 +122,10 @@ class ScheduleScreenViewModel extends BaseViewModel {
       _filter.idType,
       _filter.id,
       DateTimeRange(
-        start: _filter.dateTimeRange.start.subtract(const Duration(days: 7)),
-        end: _filter.dateTimeRange.end.subtract(const Duration(days: 7)),
+        start: _filter.dateTimeRange.start
+            .subtract(const Duration(days: DateTime.daysPerWeek)),
+        end: _filter.dateTimeRange.end
+            .subtract(const Duration(days: DateTime.daysPerWeek)),
       ),
     );
     _updateScheduleLoader();
@@ -152,7 +160,7 @@ class ScheduleScreenViewModel extends BaseViewModel {
     if (schedule == null) {
       throw Exception('Schedule was null');
     }
-    
+
     final result = SplayTreeMap<int, List<Subject>>();
     for (Subject subject in schedule) {
       if (!result.keys.contains(subject.dateTimeRange.start.weekday)) {
@@ -202,9 +210,9 @@ class ScheduleScreenViewModel extends BaseViewModel {
   Future<List<ScheduleSearchResultItem>> getSearchSuggestions(
       String value) async {
     final suggestions = await tryLoginAndRetrieveData(
-      () async => await _searchIdOnPortalService.findIDOnPortal(value, _idType),
-      () async => <ScheduleSearchResultItem>[]
-    );
+        () async =>
+            await _searchIdOnPortalService.findIDOnPortal(value, _idType),
+        () async => <ScheduleSearchResultItem>[]);
 
     return suggestions;
   }
