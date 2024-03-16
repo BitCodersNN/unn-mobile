7d3335a94feea0a77bbcf875b3c36d18ecd76d4d
Mikhail Kriseev <mish.kryseev@gmail.com>
2024-03-16 21:40:45 +0300

    Вынес диалоги в отдельную директорию

---
+++ lib/ui/widgets/dialogs/radio_group_dialog.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class RadioGroupDialog extends StatefulWidget {
+  final List<Widget> radioLabels;
+  final Widget? label;
+  final Widget okButtonChild;
+  final Widget cancelButtonChild;
+  const RadioGroupDialog({
+    super.key,
+    required this.radioLabels,
+    this.label,
+    this.okButtonChild = const Text("OK"),
+    this.cancelButtonChild = const Text("Отмена"),
+  });
+
+  @override
+  State<RadioGroupDialog> createState() => _RadioGroupDialogState();
+}
+
+class _RadioGroupDialogState extends State<RadioGroupDialog> {
+  int? selectedValue = 0;
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: widget.label,
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: List.generate(
+          widget.radioLabels.length,
+          (index) => RadioListTile<int>(
+            title: widget.radioLabels[index],
+            value: index,
+            groupValue: selectedValue,
+            onChanged: (value) {
+              setState(
+                () {
+                  selectedValue = value;
+                },
+              );
+            },
+          ),
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: widget.cancelButtonChild,
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context, selectedValue);
+          },
+          child: widget.okButtonChild,
+        ),
+      ],
+    );
+  }
+}
