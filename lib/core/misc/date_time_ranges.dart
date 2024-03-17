e3a4a6638ffb86ec0c0f1ad3d7f35f7ca61de7dd
Yurin Andrey <100288192+Namxobick@users.noreply.github.com>
2024-03-17 22:18:06 +0300

    Исправил ошибку в DateTimeRanges.untilEndOfWeek + рефакторинг (#294)

--- lib/core/misc/date_time_ranges.dart
+++ lib/core/misc/date_time_ranges.dart
@@ -1,63 +1,69 @@
 import 'package:flutter/material.dart';
 
-class DateTimeRanges{
-  static DateTimeRange currentWeek(){
-    final startOfWeek = DateTime.now().subtract(Duration(days: DateTime.now().weekday - 1));
-    final endOfWeek = startOfWeek.add(const Duration(days: 6));
+class DateTimeRanges {
+  static DateTimeRange currentWeek() {
+    final startOfWeek =
+        DateTime.now().subtract(Duration(days: DateTime.now().weekday - 1));
+    final endOfWeek =
+        startOfWeek.add(const Duration(days: DateTime.daysPerWeek - 1));
 
     return DateTimeRange(start: startOfWeek, end: endOfWeek);
   }
 
   static DateTimeRange nextWeek() {
-    final startOfWeek = DateTime.now().add(Duration(days: 8 - DateTime.now().weekday));
-    final endOfWeek = startOfWeek.add(const Duration(days: 6));
-    
+    final startOfWeek = DateTime.now()
+        .add(Duration(days: DateTime.daysPerWeek - DateTime.now().weekday + 1));
+    final endOfWeek =
+        startOfWeek.add(const Duration(days: DateTime.daysPerWeek - 1));
+
     return DateTimeRange(start: startOfWeek, end: endOfWeek);
   }
 
-  static DateTimeRange currentMonth(){
+  static DateTimeRange currentMonth() {
     final now = DateTime.now();
-    final endOfMonth = DateTime(now.year, now.month + 1, 1).subtract(const Duration(days: 1));
-    
-    return DateTimeRange(start: DateTime(now.year, now.month, 1), end: endOfMonth);
+    final endOfMonth =
+        DateTime(now.year, now.month + 1, 1).subtract(const Duration(days: 1));
+
+    return DateTimeRange(
+        start: DateTime(now.year, now.month, 1), end: endOfMonth);
   }
 
-  static DateTimeRange currentSemester(){
+  static DateTimeRange currentSemester() {
     final now = DateTime.now();
     DateTime startOfSemester, endOfSemester;
-    if (2 <= now.month && now.month < 9){
-      startOfSemester = DateTime(now.year, 2, 1);
-      endOfSemester = DateTime(now.year, 8, 1);
-    }
-    else{
-      startOfSemester = DateTime(now.year, 9, 1);
-      endOfSemester = DateTime(now.year + 1, 2, 1);
+    if (DateTime.february <= now.month && now.month < DateTime.september) {
+      startOfSemester = DateTime(now.year, DateTime.february, 1);
+      endOfSemester = DateTime(now.year, DateTime.july, 15);
+    } else {
+      startOfSemester = DateTime(now.year, DateTime.september, 1);
+      endOfSemester = DateTime(now.year + 1, DateTime.february, 1);
     }
     return DateTimeRange(start: startOfSemester, end: endOfSemester);
   }
 
-  static DateTimeRange untilEndOfWeek(){
+  static DateTimeRange untilEndOfWeek() {
     DateTime now = DateTime.now();
-    DateTime endOfWeek = now.add(Duration(days: DateTime.saturday - now.weekday));
+    DateTime endOfWeek =
+        now.add(Duration(days: DateTime.daysPerWeek - now.weekday));
 
     return DateTimeRange(start: now, end: endOfWeek);
   }
 
-  static DateTimeRange untilEndOfMonth(){
+  static DateTimeRange untilEndOfMonth() {
     DateTime now = DateTime.now();
-    final endOfMonth = DateTime(now.year, now.month + 1, 1).subtract(const Duration(days: 1));
+    final endOfMonth =
+        DateTime(now.year, now.month + 1, 1).subtract(const Duration(days: 1));
 
     return DateTimeRange(start: now, end: endOfMonth);
   }
 
-  static DateTimeRange untilEndOfSemester(){
+  static DateTimeRange untilEndOfSemester() {
     final now = DateTime.now();
     DateTime endOfSemester;
-    if (2 <= now.month && now.month < 9){
-      endOfSemester = DateTime(now.year, 8, 1);
-    }
-    else{
-      endOfSemester = DateTime(now.year + 1, 2, 1);
+    if (DateTime.february <= now.month && now.month < DateTime.september) {
+      endOfSemester = DateTime(now.year, DateTime.july, 15);
+    } else {
+      endOfSemester = DateTime(now.year + 1, DateTime.february, 1);
     }
     return DateTimeRange(start: now, end: endOfSemester);
   }
