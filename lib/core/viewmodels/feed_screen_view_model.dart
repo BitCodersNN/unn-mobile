b2b97af732ff5afce8eae4bf58a91c24ffdb7421
Mikhail Kriseev <mish.kryseev@gmail.com>
2024-03-19 21:34:17 +0300

    Демка ленты

--- lib/core/viewmodels/feed_screen_view_model.dart
+++ lib/core/viewmodels/feed_screen_view_model.dart
@@ -1,6 +1,17 @@
+import 'package:injector/injector.dart';
+import 'package:unn_mobile/core/models/blog_data.dart';
+import 'package:unn_mobile/core/services/interfaces/getting_blog_posts.dart';
 import 'package:unn_mobile/core/viewmodels/base_view_model.dart';
 
 
 
 
-class FeedScreenViewModel extends BaseViewModel {}
+class FeedScreenViewModel extends BaseViewModel {
+  final GettingBlogPosts _gettingBlogPosts = Injector.appInstance.get<GettingBlogPosts>();
+  Future<List<BlogData>?>? blogPosts;
+
+
+  void init() {
+    blogPosts = _gettingBlogPosts.getBlogPosts();
+  }
+}
--- lib/ui/views/main_page/feed/feed.dart
+++ lib/ui/views/main_page/feed/feed.dart
@@ -1,21 +1,25 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bbcode/flutter_bbcode.dart';
+import 'package:unn_mobile/core/misc/customBBTags.dart';
 import 'package:unn_mobile/core/viewmodels/feed_screen_view_model.dart';
 import 'package:unn_mobile/ui/views/base_view.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class FeedScreenView extends StatelessWidget {
   const FeedScreenView({super.key});
 
+  final data2 = "someText [url]example.com[/url] [URL]example.com[/URL]";
   @override
   Widget build(BuildContext context) {
     return BaseView<FeedScreenViewModel>(
       builder: (context, value, child) {
-
         return const Center(
           child: Image(
             image: AssetImage("assets/images/chick_in_the_egg.png"),
           ),
         );
-
       },
     );
   }
