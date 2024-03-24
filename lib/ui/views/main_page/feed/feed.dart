e398758b712c8b096edee89f4a01113e8ffc808a
Daria Sokolova <dasa.vzn@mail.ru>
2024-03-24 22:54:04 +0300

    Убрала ненужную функцию

--- lib/ui/views/main_page/feed/feed.dart
+++ lib/ui/views/main_page/feed/feed.dart
@@ -11,40 +11,7 @@ import 'package:intl/intl.dart';
 
 class FeedScreenView extends StatelessWidget {
   const FeedScreenView({Key? key});
-
   final data2 = "someText [url]example.com[/url] [URL]example.com[/URL]";
-
-  String _getMonthName(int month) {
-    switch (month) {
-      case 1:
-        return 'января';
-      case 2:
-        return 'февраля';
-      case 3:
-        return 'марта';
-      case 4:
-        return 'апреля';
-      case 5:
-        return 'мая';
-      case 6:
-        return 'июня';
-      case 7:
-        return 'июля';
-      case 8:
-        return 'августа';
-      case 9:
-        return 'сентября';
-      case 10:
-        return 'октября';
-      case 11:
-        return 'ноября';
-      case 12:
-        return 'декабря';
-      default:
-        return '';
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return BaseView<FeedScreenViewModel>(
@@ -119,8 +86,8 @@ class FeedScreenView extends StatelessWidget {
                                               ),
                                             ),
                                             Text(
-                                              DateFormat(
-                                                      'd MMMM, HH:mm', 'ru_RU')
+                                              DateFormat('d MMMM yyyy, HH:mm',
+                                                      'ru_RU')
                                                   .format(post.datePublish),
                                               style: TextStyle(
                                                 fontSize: 13,
