7ff578843fc4036d1cd51008217f8967fb098492
Mikhail Kriseev <mish.kryseev@gmail.com>
2024-04-01 23:56:44 +0300

    Вьюмодель экрана ленты (#302)

--- lib/core/viewmodels/feed_screen_view_model.dart
+++ lib/core/viewmodels/feed_screen_view_model.dart
@@ -1,6 +1,27 @@
+import 'package:injector/injector.dart';
+import 'package:unn_mobile/core/models/post_with_loaded_info.dart';
+import 'package:unn_mobile/core/services/interfaces/feed_stream_updater_service.dart';
 import 'package:unn_mobile/core/viewmodels/base_view_model.dart';
 
+class FeedScreenViewModel extends BaseViewModel {
+  final FeedUpdaterService _feedStreamUpdater =
+      Injector.appInstance.get<FeedUpdaterService>();
 
+  List<PostWithLoadedInfo> get posts => _feedStreamUpdater.feedPosts;
 
+  bool get isLoadingPosts => _feedStreamUpdater.isBusy;
 
-class FeedScreenViewModel extends BaseViewModel {}
+  void init() {
+    _feedStreamUpdater.addListener(() {
+      notifyListeners();
+    });
+  }
+
+  Future<void> updateFeed() async {
+    await _feedStreamUpdater.updateFeed();
+  }
+
+  void loadNextPage() {
+    _feedStreamUpdater.loadNextPage();
+  }
+}
