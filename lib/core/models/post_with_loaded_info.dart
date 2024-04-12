bc1a836943a2ae0ef3a34b57245486f06484ee6b
Yurin Andrey <100288192+Namxobick@users.noreply.github.com>
2024-04-12 21:21:23 +0700

    Получение информации о файлах, прикрепленных к посту (#314)

--- lib/core/models/post_with_loaded_info.dart
+++ lib/core/models/post_with_loaded_info.dart
@@ -1,21 +1,29 @@
 import 'package:unn_mobile/core/models/blog_data.dart';
+import 'package:unn_mobile/core/models/file_data.dart';
 import 'package:unn_mobile/core/models/user_data.dart';
 
 class KeysForPostWithLoadedInfoJsonConverter {
   static const String author = 'author';
   static const String post = 'post';
+  static const String files = 'files';
 }
 
 class PostWithLoadedInfo {
   final UserData _author;
   final BlogData _post;
+  final List<FileData> _files;
 
-  PostWithLoadedInfo({required UserData author, required BlogData post})
-      : _author = author,
-        _post = post;
+  PostWithLoadedInfo({
+    required UserData author,
+    required BlogData post,
+    required List<FileData> files,
+  })  : _author = author,
+        _post = post,
+        _files = files;
 
   UserData get author => _author;
   BlogData get post => _post;
+  List<FileData> get files => _files;
 
   factory PostWithLoadedInfo.fromJson(Map<String, Object?> jsonMap) {
     return PostWithLoadedInfo(
@@ -25,11 +33,17 @@ class PostWithLoadedInfo {
       post: BlogData.fromJson(
           jsonMap[KeysForPostWithLoadedInfoJsonConverter.post]
               as Map<String, Object?>),
+      files: (jsonMap[KeysForPostWithLoadedInfoJsonConverter.files]
+              as List<dynamic>)
+          .map((element) => FileData.fromJson(element))
+          .toList(),
     );
   }
 
   Map<String, dynamic> toJson() => {
-        KeysForPostWithLoadedInfoJsonConverter.author: author.toJson(),
-        KeysForPostWithLoadedInfoJsonConverter.post: post.toJson(),
+        KeysForPostWithLoadedInfoJsonConverter.author: _author.toJson(),
+        KeysForPostWithLoadedInfoJsonConverter.post: _post.toJson(),
+        KeysForPostWithLoadedInfoJsonConverter.files:
+            files.map((file) => file.toJson()).toList(),
       };
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ import 'package:unn_mobile/core/services/implementations/getting_blog_post_comme
 import 'package:unn_mobile/core/services/implementations/export_schedule_service_impl.dart';
 import 'package:unn_mobile/core/services/implementations/feed_stream_updater_service_impl.dart';
 import 'package:unn_mobile/core/services/implementations/getting_blog_posts_impl.dart';
+import 'package:unn_mobile/core/services/implementations/getting_file_data_impl.dart';
 import 'package:unn_mobile/core/services/implementations/getting_profile_impl.dart';
 import 'package:unn_mobile/core/services/implementations/getting_profile_of_current_user_service_impl.dart';
 import 'package:unn_mobile/core/services/implementations/getting_schedule_service_impl.dart';
@@ -31,6 +32,7 @@ import 'package:unn_mobile/core/services/interfaces/getting_blog_post_comments.d
 import 'package:unn_mobile/core/services/interfaces/export_schedule_service.dart';
 import 'package:unn_mobile/core/services/interfaces/feed_stream_updater_service.dart';
 import 'package:unn_mobile/core/services/interfaces/getting_blog_posts.dart';
+import 'package:unn_mobile/core/services/interfaces/getting_file_data.dart';
 import 'package:unn_mobile/core/services/interfaces/getting_profile.dart';
 import 'package:unn_mobile/core/services/interfaces/getting_profile_of_current_user_service.dart';
 import 'package:unn_mobile/core/services/interfaces/getting_schedule_service.dart';
@@ -104,6 +106,7 @@ void registerDependencies() {
   injector.registerSingleton<TypeOfCurrentUser>(() => TypeOfCurrentUser());
   injector.registerSingleton<PostWithLoadedInfoProvider>(
       () => PostWithLoadedInfoProviderImpl());
+  injector.registerSingleton<GettingFileData>(() => GettingFileDataImpl());
 
   injector.registerDependency(() => LoadingPageViewModel());
   injector.registerDependency(() => AuthPageViewModel());
