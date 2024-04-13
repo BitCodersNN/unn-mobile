024c377b459b9a4f9325337ab63874de8a778943
Mikhail Kriseev <mish.kryseev@gmail.com>
2024-04-14 02:30:02 +0300

    Вложения (скелет)

--- lib/ui/views/main_page/feed/feed.dart
+++ lib/ui/views/main_page/feed/feed.dart
@@ -1,16 +1,27 @@
+import 'dart:io';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bbcode/flutter_bbcode.dart';
 import 'package:html_unescape/html_unescape.dart';
+import 'package:injector/injector.dart';
+import 'package:intl/intl.dart';
+import 'package:open_filex/open_filex.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
+import 'package:photo_view/photo_view.dart';
 import 'package:unn_mobile/core/misc/custom_bb_tags.dart' as custom_tags;
+import 'package:unn_mobile/core/misc/size_converter.dart';
 import 'package:unn_mobile/core/misc/user_functions.dart';
+import 'package:unn_mobile/core/models/file_data.dart';
 import 'package:unn_mobile/core/models/post_with_loaded_info.dart';
 import 'package:unn_mobile/core/models/user_data.dart';
+import 'package:unn_mobile/core/services/interfaces/authorisation_service.dart';
 import 'package:unn_mobile/core/viewmodels/feed_screen_view_model.dart';
 import 'package:unn_mobile/ui/unn_mobile_colors.dart';
 import 'package:unn_mobile/ui/views/base_view.dart';
+import 'package:unn_mobile/ui/widgets/dialogs/message_dialog.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:intl/intl.dart';
 
 class FeedScreenView extends StatelessWidget {
   const FeedScreenView({super.key});
@@ -34,7 +45,8 @@ class FeedScreenView extends StatelessWidget {
                     ),
                   );
                 }
-                return feedPost(theme, model, model.posts[index], unescaper);
+                return feedPost(
+                    theme, model, model.posts[index], unescaper, context);
               },
             ),
           ),
@@ -59,6 +71,7 @@ class FeedScreenView extends StatelessWidget {
     FeedScreenViewModel model,
     PostWithLoadedInfo post,
     HtmlUnescape unescaper,
+    BuildContext context,
   ) {
     return Container(
       margin: const EdgeInsets.only(top: 12),
@@ -131,6 +144,14 @@ class FeedScreenView extends StatelessWidget {
               );
             },
           ),
+          for (final file in post.files)
+            AttachedFile(
+              fileData: file,
+            ),
+          SizedBox(
+            width: double.infinity,
+            child: Text("Комментарии: ${post.post.numberOfComments}"),
+          ),
         ],
       ),
     );
@@ -187,3 +208,202 @@ class FeedScreenView extends StatelessWidget {
   }
 }
 
+class AttachedFile extends StatefulWidget {
+  final FileData _fileData;
+  const AttachedFile({super.key, required fileData}) : _fileData = fileData;
+
+  @override
+  State<AttachedFile> createState() => _AttachedFileState();
+}
+
+class _AttachedFileState extends State<AttachedFile> {
+  Future<String?> getDownloadPath() async {
+    Directory? directory;
+    try {
+      if (Platform.isIOS) {
+        directory = await getApplicationDocumentsDirectory();
+      } else {
+        directory = Directory('/storage/emulated/0/Download');
+        // Put file in global download folder, if for an unknown reason it didn't exist, we fallback
+        if (!await directory.exists()) {
+          directory = await getExternalStorageDirectory();
+        }
+        if (directory != null) {
+          directory = Directory('${directory.path}/unnmobile');
+          if (!(await directory.exists())) {
+            await directory.create();
+          }
+        }
+      }
+    } catch (err, stack) {
+      FirebaseCrashlytics.instance
+          .recordError("Cannot get download folder path: $err", stack);
+    }
+    return directory?.path;
+  }
+
+  FileData get fileData => widget._fileData;
+
+  static Map<String, Future<File?>> downloadingFiles = {};
+
+  Future<File?> downloadFileWrapper() async {
+    if (downloadingFiles.containsKey(fileData.downloadUrl)) {
+      return await downloadingFiles[fileData.downloadUrl];
+    }
+    downloadingFiles.putIfAbsent(fileData.downloadUrl, () => downloadFile());
+    File? file = await downloadingFiles[fileData.downloadUrl];
+    downloadingFiles.remove(fileData.downloadUrl);
+    return file;
+  }
+
+  Future<File?> downloadFile() async {
+    final authService = Injector.appInstance.get<AuthorisationService>();
+    if (authService.sessionId == null) {
+      return null;
+    }
+    String? downloadsPath = await getDownloadPath();
+    if (downloadsPath == null) {
+      return null;
+    }
+    final storedFile = File('$downloadsPath/${fileData.name}');
+    if (!storedFile.existsSync()) {
+      HttpClient client = HttpClient();
+      try {
+        final request =
+            await client.openUrl('get', Uri.parse(fileData.downloadUrl));
+        request.cookies.add(Cookie('PHPSESSID', authService.sessionId!));
+        final response = await request.close();
+        if (response.statusCode == 200) {
+          final bytes = await consolidateHttpClientResponseBytes(response);
+          await storedFile.writeAsBytes(bytes);
+        }
+      } catch (error, stack) {
+        FirebaseCrashlytics.instance.recordError(error, stack);
+      }
+    }
+    return storedFile;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final sizeConverter = SizeConverter();
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder(
+            future: downloadingFiles[fileData.downloadUrl],
+            builder: (context, snapshot) {
+              return Row(
+                children: [
+                  if (snapshot.connectionState != ConnectionState.none &&
+                      !snapshot.hasData)
+                    const CircularProgressIndicator(),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () async {
+                        if (downloadingFiles
+                            .containsKey(fileData.downloadUrl)) {
+                          return;
+                        }
+                        switch (path.extension(fileData.name.toLowerCase())) {
+                          case '.jpg':
+                          case '.png':
+                          case '.jpeg':
+                          case '.gif':
+                          case '.webp':
+                            if (context.mounted) {
+                              await showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return Dismissible(
+                                    key: ValueKey(fileData.downloadUrl),
+                                    onDismissed: (direction) {
+                                      Navigator.of(context).pop();
+                                    },
+                                    direction: DismissDirection.vertical,
+                                    child: FutureBuilder(
+                                      future: downloadFileWrapper(),
+                                      builder: (context, snapshot) {
+                                        return snapshot.connectionState ==
+                                                    ConnectionState.done &&
+                                                snapshot.hasData
+                                            ? SizedBox(
+                                                width: double.infinity,
+                                                child: PhotoView(
+                                                  imageProvider:
+                                                      FileImage(snapshot.data!),
+                                                ),
+                                              )
+                                            : const Center(
+                                                child:
+                                                    CircularProgressIndicator(),
+                                              );
+                                      },
+                                    ),
+                                  );
+                                },
+                              );
+                            }
+                            break;
+                          default:
+                            final f = await downloadFileWrapper();
+                            if (f != null) {
+                              final openResult = await OpenFilex.open(f.path);
+                              switch (openResult.type) {
+                                case ResultType.error:
+                                  if (context.mounted) {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content: Text("Неизвестная ошибка"),
+                                      ),
+                                    );
+                                  }
+                                  break;
+                                case ResultType.noAppToOpen:
+                                  if (context.mounted) {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content:
+                                            Text("Нет подходящей программы"),
+                                      ),
+                                    );
+                                  }
+                                  break;
+                                case ResultType.permissionDenied:
+                                  if (context.mounted) {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content: Text("Нет доступа к файлам"),
+                                      ),
+                                    );
+                                  }
+                                  break;
+                                default:
+                                  break;
+                              }
+                            }
+                            break;
+                        }
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            fileData.name,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Text(
+                            '${path.extension(fileData.name).substring(1)} | ${sizeConverter.convertBytesToSize(fileData.sizeInBytes)} ${sizeConverter.lastUsedUnit}',
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              );
+            }),
+      ),
+    );
+  }
+}
