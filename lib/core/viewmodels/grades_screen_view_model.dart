6924e155a0b4a80f3cf6fe03c8b2314938ea2869
Mikhail Kriseev <mish.kryseev@gmail.com>
2024-05-05 22:13:47 +0300

    tryLoginAndRetrieveData + сообщение о незагруженной зачётке

--- lib/core/viewmodels/grades_screen_view_model.dart
+++ lib/core/viewmodels/grades_screen_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:injector/injector.dart';
+import 'package:unn_mobile/core/misc/try_login_and_retrieve_data.dart';
 import 'package:unn_mobile/core/models/mark_by_subject.dart';
 import 'package:unn_mobile/core/services/interfaces/getting_grade_book.dart';
 import 'package:unn_mobile/core/services/interfaces/mark_by_subject_provider.dart';
@@ -10,13 +11,10 @@ class GradesScreenViewModel extends BaseViewModel {
   final MarkBySubjectProvider _markBySubjectProvider =
       Injector.appInstance.get<MarkBySubjectProvider>();
 
-  Future<Map<int, List<MarkBySubject>>?> getGradeBook({
-    bool offline = false,
-  }) async {
-    if (offline) {
-      return _markBySubjectProvider.getData();
-    } else {
-      return await _gradeBookService.getGradeBook();
-    }
+  Future<Map<int, List<MarkBySubject>>?> getGradeBook() async {
+    return await tryLoginAndRetrieveData(
+      () async => await _gradeBookService.getGradeBook(),
+      () async => _markBySubjectProvider.getData(),
+    );
   }
 }
--- lib/ui/views/main_page/grades/grades.dart
+++ lib/ui/views/main_page/grades/grades.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:unn_mobile/core/models/mark_by_subject.dart';
 import 'package:unn_mobile/core/viewmodels/grades_screen_view_model.dart';
-import 'package:unn_mobile/ui/builders/online_status_stream_builder.dart';
 import 'package:unn_mobile/ui/views/base_view.dart';
 
 class GradesScreenView extends StatelessWidget {
@@ -11,24 +10,18 @@ class GradesScreenView extends StatelessWidget {
   Widget build(BuildContext context) {
     return BaseView<GradesScreenViewModel>(
       builder: (context, value, child) {
-        return OnlineStatusStreamBuilder(
-          onlineWidget:
-              _getGradesBook(context: context, model: value, offline: false),
-          offlineWidget:
-              _getGradesBook(context: context, model: value, offline: true),
-        );
+        return _getGradesBook(context: context, model: value);
       },
     );
   }
 
   Widget _getGradesBook({
-    required bool offline,
     required GradesScreenViewModel model,
     required BuildContext context,
   }) {
     final theme = Theme.of(context);
     return FutureBuilder(
-      future: model.getGradeBook(offline: offline),
+      future: model.getGradeBook(),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
           return const Center(
@@ -38,120 +31,127 @@ class GradesScreenView extends StatelessWidget {
             ),
           );
         }
-        if (snapshot.hasData && snapshot.data != null) {
-          final tabs = snapshot.data!.keys.toList();
-          tabs.sort();
-          return DefaultTabController(
-            initialIndex: tabs.length - 1,
-            length: tabs.length,
-            child: Column(
-              children: [
-                Container(
-                  color: theme.colorScheme.background,
-                  child: TabBar.secondary(
-                    tabAlignment: TabAlignment.start,
-                    enableFeedback: false,
-                    isScrollable: true,
-                    tabs: [
-                      for (final tab in tabs)
-                        Tab(
-                          child: Text('Семестр $tab'),
-                        ),
-                    ],
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasData) {
+            final tabs = snapshot.data!.keys.toList();
+            tabs.sort();
+            return DefaultTabController(
+              initialIndex: tabs.length - 1,
+              length: tabs.length,
+              child: Column(
+                children: [
+                  Container(
+                    color: theme.colorScheme.background,
+                    child: TabBar.secondary(
+                      tabAlignment: TabAlignment.start,
+                      enableFeedback: false,
+                      isScrollable: true,
+                      tabs: [
+                        for (final tab in tabs)
+                          Tab(
+                            child: Text('Семестр $tab'),
+                          ),
+                      ],
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: TabBarView(
-                    children: [
-                      for (final tab in tabs)
-                        SingleChildScrollView(
-                          physics: const AlwaysScrollableScrollPhysics(),
-                          child: Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: Table(
-                              border: TableBorder.all(
-                                borderRadius: const BorderRadius.all(
-                                  Radius.circular(4.0),
-                                ),
-                              ),
-                              children: [
-                                TableRow(
-                                  decoration: BoxDecoration(
-                                    color: theme.highlightColor,
+                  Expanded(
+                    child: TabBarView(
+                      children: [
+                        for (final tab in tabs)
+                          SingleChildScrollView(
+                            physics: const AlwaysScrollableScrollPhysics(),
+                            child: Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Table(
+                                border: TableBorder.all(
+                                  borderRadius: const BorderRadius.all(
+                                    Radius.circular(4.0),
                                   ),
-                                  children: const [
-                                    TableCell(
-                                      child: Padding(
-                                        padding: EdgeInsets.all(8.0),
-                                        child: Text(
-                                          'Дисциплина',
-                                          textAlign: TextAlign.center,
-                                        ),
-                                      ),
-                                    ),
-                                    TableCell(
-                                      child: Padding(
-                                        padding: EdgeInsets.all(8.0),
-                                        child: Text(
-                                          'Вид контроля',
-                                          textAlign: TextAlign.center,
-                                        ),
-                                      ),
-                                    ),
-                                    TableCell(
-                                      child: Padding(
-                                        padding: EdgeInsets.all(8.0),
-                                        child: Text(
-                                          'Оценка',
-                                          textAlign: TextAlign.center,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
                                 ),
-                                for (final row in snapshot.data![tab]!)
+                                children: [
                                   TableRow(
-                                    children: [
+                                    decoration: BoxDecoration(
+                                      color: theme.highlightColor,
+                                    ),
+                                    children: const [
                                       TableCell(
                                         child: Padding(
-                                          padding: const EdgeInsets.all(8.0),
+                                          padding: EdgeInsets.all(8.0),
                                           child: Text(
-                                            row.subject,
+                                            'Дисциплина',
                                             textAlign: TextAlign.center,
                                           ),
                                         ),
                                       ),
                                       TableCell(
                                         child: Padding(
-                                          padding: const EdgeInsets.all(8.0),
+                                          padding: EdgeInsets.all(8.0),
                                           child: Text(
-                                            row.controlType,
+                                            'Вид контроля',
                                             textAlign: TextAlign.center,
                                           ),
                                         ),
                                       ),
                                       TableCell(
                                         child: Padding(
-                                          padding: const EdgeInsets.all(8.0),
+                                          padding: EdgeInsets.all(8.0),
                                           child: Text(
-                                            row.markType.convertToString(),
+                                            'Оценка',
                                             textAlign: TextAlign.center,
                                           ),
                                         ),
                                       ),
                                     ],
                                   ),
-                              ],
+                                  for (final row in snapshot.data![tab]!)
+                                    TableRow(
+                                      children: [
+                                        TableCell(
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              row.subject,
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ),
+                                        ),
+                                        TableCell(
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              row.controlType,
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ),
+                                        ),
+                                        TableCell(
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              row.markType.convertToString(),
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-          );
+                ],
+              ),
+            );
+          } else {
+            return const Center(
+              child: Text('Ещё нет загруженной версии зачётной книжки'),
+            );
+          }
         }
+
         return const Center(
           child: SizedBox(
             height: 100.0,
