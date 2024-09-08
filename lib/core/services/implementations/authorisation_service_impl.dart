152159ab9c748a80dde96c609852fcbb69f990b1
Yurin Andrey <100288192+Namxobick@users.noreply.github.com>
2024-09-08 21:45:12 +0300

    Удалил лишний пробел

--- lib/core/services/implementations/authorisation_service_impl.dart
+++ lib/core/services/implementations/authorisation_service_impl.dart
@@ -38,7 +38,7 @@ class AuthorizationServiceImpl implements AuthorizationService {
     _isAuthorised = false;
 
     if (await _isOffline()) {
-       _onlineStatus.isOnline = false;
+      _onlineStatus.isOnline = false;
       return AuthRequestResult.noInternet;
     }
 
