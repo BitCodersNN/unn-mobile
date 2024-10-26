6b2503ae42a1271ddfe02c7ea2505176453637c6
Yurin Andrey <andreyyurin@icloud.com>
2024-10-26 13:35:46 +0300

    Удалил неисползуемый файл type_defs

--- lib/core/misc/type_defs.dart
+++
@@ -1,8 +0,0 @@
-import 'package:unn_mobile/core/misc/lru_cache.dart';
-import 'package:unn_mobile/core/models/file_data.dart';
-import 'package:unn_mobile/core/models/rating_list.dart';
-import 'package:unn_mobile/core/models/user_data.dart';
-
-typedef LRUCacheUserData = LRUCache<int, UserData>;
-typedef LRUCacheLoadedFile = LRUCache<int, FileData>;
-typedef LRUCacheRatingList = LRUCache<int, RatingList>;
