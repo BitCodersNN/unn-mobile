466f3aca3d021c7f7f29e45787b3a768e0684329
Yurin Andrey <andreyyurin@icloud.com>
2025-03-02 16:29:49 +0300

    автоформатирование

--- lib/core/misc/options_factory/options_with_timeout_and_expected_type_factory.dart
+++ lib/core/misc/options_factory/options_with_timeout_and_expected_type_factory.dart
@@ -4,9 +4,11 @@ import 'package:unn_mobile/core/misc/options_factory/options_with_expected_type_
 
 class OptionsWithTimeoutAndExpectedTypeFactory {
   static Options options(int duration, ResponseDataType responseDataType) =>
-  Options(
-    sendTimeout: Duration(seconds: duration),
-    receiveTimeout: Duration(seconds: duration),
-    extra: OptionsWithExpectedTypeFactory.mapWithExpectedType(responseDataType),
-  );
+      Options(
+        sendTimeout: Duration(seconds: duration),
+        receiveTimeout: Duration(seconds: duration),
+        extra: OptionsWithExpectedTypeFactory.mapWithExpectedType(
+          responseDataType,
+        ),
+      );
 }
