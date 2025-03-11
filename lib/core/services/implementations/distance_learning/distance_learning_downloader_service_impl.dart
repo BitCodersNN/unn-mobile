9c7c1bd822ad72fc31b3b1a1f84235260205090c
Yurin Andrey <andreyyurin@icloud.com>
2025-03-11 11:20:26 +0300

    Реализовал

---
+++ lib/core/services/implementations/distance_learning/distance_learning_downloader_service_impl.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+import 'package:unn_mobile/core/misc/file_helpers/file_downloader.dart';
+import 'package:unn_mobile/core/services/interfaces/distance_learning/distance_learning_downloader_service.dart';
+
+class DistanceLearningDownloaderServiceImpl
+    implements DistanceLearningDownloaderService {
+  final FileDownloader _fileDownloader;
+
+  DistanceLearningDownloaderServiceImpl(
+    loggerService,
+    apiHelper,
+  ) : _fileDownloader = FileDownloader(
+          loggerService,
+          apiHelper,
+          downloadFolderName: 'distanceLearning',
+        );
+
+  @override
+  Future<File?> downloadFile({
+    required String fileName,
+    required String downloadUrl,
+    bool force = false,
+  }) {
+    return _fileDownloader.downloadFile(
+      fileName,
+      downloadUrl: downloadUrl,
+      force: force,
+    );
+  }
+}
