0e11c7110fa3f3f2f2a790e7830a9ce52939813e
Yurin Andrey <andreyyurin@icloud.com>
2025-03-22 15:55:31 +0300

    Автоформатирование

--- lib/core/misc/hex_color.dart
+++ lib/core/misc/hex_color.dart
@@ -19,8 +19,7 @@ extension ColorParser on Color {
   }
 
   /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
-  String toHex({bool leadingHashSign = true}) => 
-      '${leadingHashSign ? '#' : ''}'
+  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
       '${a.toInt().toRadixString(16)}'
       '${r.toInt().toRadixString(16).padLeft(2, '0')}'
       '${g.toInt().toRadixString(16).padLeft(2, '0')}'
