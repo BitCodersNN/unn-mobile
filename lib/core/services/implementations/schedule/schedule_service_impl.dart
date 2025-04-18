e79738a80cb589b7cdc4111a915368994cf5de56
Yurin Andrey <andreyyurin@icloud.com>
2025-04-18 14:20:01 +0300

    Удалил неиспользуемые импорты

--- lib/core/services/implementations/schedule/schedule_service_impl.dart
+++ lib/core/services/implementations/schedule/schedule_service_impl.dart
@@ -3,10 +3,8 @@ import 'package:unn_mobile/core/constants/api/path.dart';
 import 'package:unn_mobile/core/misc/api_helpers/api_helper.dart';
 import 'package:unn_mobile/core/misc/dio_options_factory/options_with_expected_type_factory.dart';
 import 'package:unn_mobile/core/misc/json_iterable_parser.dart';
-import 'package:unn_mobile/core/models/dialog/dialog_query_parameter.dart';
 import 'package:unn_mobile/core/models/schedule/schedule_filter.dart';
 import 'package:unn_mobile/core/models/schedule/subject.dart';
-import 'package:unn_mobile/core/services/implementations/dialog/dialog_service_impl.dart';
 import 'package:unn_mobile/core/services/interfaces/schedule/schedule_service.dart';
 import 'package:unn_mobile/core/services/interfaces/common/logger_service.dart';
 
