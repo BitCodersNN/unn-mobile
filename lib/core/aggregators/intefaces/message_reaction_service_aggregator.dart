35e27abbdef63533231329b4a8a65d44084280a2
Yurin Andrey <andreyyurin@icloud.com>
2025-04-23 18:51:14 +0300

    Информация о лицензии

--- lib/core/aggregators/intefaces/message_reaction_service_aggregator.dart
+++ lib/core/aggregators/intefaces/message_reaction_service_aggregator.dart
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2025 BitCodersNN
+
 import 'package:unn_mobile/core/services/interfaces/dialog/message/reaction/message_reaction_fetcher_service.dart';
 import 'package:unn_mobile/core/services/interfaces/dialog/message/reaction/message_reaction_mutator_service.dart';
 
