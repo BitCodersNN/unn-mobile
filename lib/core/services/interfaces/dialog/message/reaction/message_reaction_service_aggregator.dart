6ab83a13d361d5c45196764b3294c0a58d16dbd1
Yurin Andrey <andreyyurin@icloud.com>
2025-04-24 21:56:00 +0300

    Удалил лишние файлы

--- lib/core/services/interfaces/dialog/message/reaction/message_reaction_service_aggregator.dart
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright 2025 BitCodersNN
-
-import 'package:unn_mobile/core/services/interfaces/dialog/message/reaction/message_reaction_fetcher_service.dart';
-import 'package:unn_mobile/core/services/interfaces/dialog/message/reaction/message_reaction_mutator_service.dart';
-
-abstract interface class MessageReactionServiceAggregator
-    implements MessageReactionFetcherService, MessageReactionMutatorService {}
