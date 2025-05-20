12ed7581d17d8ac871c634f7568472c4a82ddae4
Mikhail Kriseev <mish.kryseev@gmail.com>
2025-05-20 15:21:52 +0300

    Более стабильное отображение профилей в ленте

--- lib/core/viewmodels/main_page/common/profile_view_model.dart
+++ lib/core/viewmodels/main_page/common/profile_view_model.dart
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2025 BitCodersNN
 
+import 'dart:async';
+
 import 'package:injector/injector.dart';
 import 'package:unn_mobile/core/misc/user/current_user_sync_storage.dart';
 import 'package:unn_mobile/core/misc/user/user_functions.dart';
@@ -66,6 +68,7 @@ class ProfileViewModel extends BaseViewModel {
     bool loadCurrentUser = false,
     bool loadFromPost = false,
     bool setBusy = true,
+    int retryAttempt = 0,
   }) {
     assert((userId != null) || loadCurrentUser);
     if (force || _loadedData == null) {
@@ -81,7 +84,20 @@ class ProfileViewModel extends BaseViewModel {
         };
       }).catchError((error, stack) {
         _loggerService.logError(error, stack);
-        _hasError = true;
+        if (retryAttempt < 5) {
+          init(
+            force: force,
+            userId: userId,
+            loadCurrentUser: loadCurrentUser,
+            loadFromPost: loadFromPost,
+            setBusy: setBusy,
+            retryAttempt: retryAttempt + 1,
+          );
+          return;
+        }
+        if (_loadedData == null) {
+          _hasError = true;
+        }
       }).whenComplete(() {
         _isLoading = false;
         notifyListeners();
