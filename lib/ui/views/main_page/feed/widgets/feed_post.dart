616d0055fe792afff8cdd7848a59498b5c8de4ac
Mikhail Kriseev <mish.kryseev@gmail.com>
2025-05-20 17:21:54 +0300

    Отображение картинок в ленте коллажем (#504)

--- lib/ui/views/main_page/feed/widgets/feed_post.dart
+++ lib/ui/views/main_page/feed/widgets/feed_post.dart
@@ -2,6 +2,7 @@
 // Copyright 2025 BitCodersNN
 
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider_plus/carousel_slider_plus.dart';
 import 'package:event/event.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
@@ -20,6 +21,7 @@ import 'package:unn_mobile/ui/views/base_view.dart';
 import 'package:unn_mobile/ui/views/main_page/feed/functions/reactions_window.dart';
 import 'package:unn_mobile/ui/views/main_page/feed/widgets/attached_file.dart';
 import 'package:unn_mobile/ui/views/main_page/main_page_routing.dart';
+import 'package:unn_mobile/ui/widgets/packed_images_view.dart';
 import 'package:unn_mobile/ui/widgets/shimmer.dart';
 import 'package:unn_mobile/ui/widgets/shimmer_loading.dart';
 
@@ -150,6 +152,11 @@ class FeedPost extends StatefulWidget {
 }
 
 class _FeedPostState extends State<FeedPost> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   void onPostRefreshError(EventArgs e) {
     const snackBar = SnackBar(
       content: Text('Не удалось обновить пост'),
@@ -221,6 +228,69 @@ class _FeedPostState extends State<FeedPost> {
                         ),
                         const SizedBox(height: 16.0),
                         FeedPost.htmlWidget(model.postText, context),
+                        PackedImagesView(
+                          onChildTap: (index) async {
+                            OverlayEntry createOverlay(int index) =>
+                                OverlayEntry(
+                                  builder: (context) {
+                                    return SafeArea(
+                                      child: Align(
+                                        alignment: Alignment.topCenter,
+                                        child: Material(
+                                          color: Colors.transparent,
+                                          child: Container(
+                                            padding: const EdgeInsets.all(12),
+                                            decoration: const BoxDecoration(
+                                              color: Colors.transparent,
+                                            ),
+                                            child: Text(
+                                              '${index + 1} из ${model.attachedImages.length}',
+                                              style: const TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 24,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                );
+
+                            var overlay = createOverlay(index);
+                            await showDialog(
+                              context: context,
+                              builder: (context) {
+                                WidgetsBinding.instance
+                                    .addPostFrameCallback((_) {
+                                  Overlay.of(context, rootOverlay: true)
+                                      .insert(overlay);
+                                });
+
+                                return ExtendedImageSlidePage(
+                                  slideAxis: SlideAxis.vertical,
+                                  child: ImagesCarousel(
+                                    viewModel: model,
+                                    imageModel: index,
+                                    onPageChanged: (index) {
+                                      overlay.remove();
+                                      overlay = createOverlay(index);
+                                      Overlay.of(context).insert(overlay);
+                                    },
+                                  ),
+                                );
+                              },
+                            );
+                            overlay.remove();
+                          },
+                          children: model.attachedImages
+                              .map(
+                                (e) => CachedNetworkImage(
+                                  imageUrl: e,
+                                ),
+                              )
+                              .toList(),
+                        ),
                         if (model.isAnnouncement)
                           FilledButton(
                             onPressed: model.markReadIfImportant,
@@ -309,6 +379,63 @@ class _FeedPostState extends State<FeedPost> {
       return unnMobileColors.importantPostHighlight;
     }
     return theme.colorScheme.surface;
+  }
+}
+
+class ImagesCarousel extends StatefulWidget {
+  final FeedPostViewModel viewModel;
+  final int imageModel;
+  final void Function(int index)? onPageChanged;
+
+  const ImagesCarousel({
+    super.key,
+    required this.viewModel,
+    this.imageModel = 0,
+    this.onPageChanged,
+  });
+
+  @override
+  State<ImagesCarousel> createState() => _ImagesCarouselState();
+}
+
+class _ImagesCarouselState extends State<ImagesCarousel> {
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      options: CarouselOptions(
+        scrollPhysics: const BouncingScrollPhysics(),
+        enableInfiniteScroll: false,
+        disableCenter: true,
+        padEnds: true,
+        viewportFraction: 1.0,
+        initialPage: widget.imageModel,
+        onPageChanged: (index, reason) => widget.onPageChanged?.call(index),
+      ),
+      items: [
+        for (final image in widget.viewModel.attachedImages)
+          ExtendedImage(
+            enableLoadState: true,
+            mode: ExtendedImageMode.gesture,
+            initGestureConfigHandler: (state) {
+              return GestureConfig(
+                minScale: 0.9,
+                animationMinScale: 0.7,
+                maxScale: 3.0,
+                animationMaxScale: 3.5,
+                speed: 1.0,
+                inertialSpeed: 100.0,
+                initialScale: 1.0,
+                inPageView: false,
+                initialAlignment: InitialAlignment.center,
+              );
+            },
+            image: CachedNetworkImageProvider(
+              image,
+            ),
+            enableSlideOutPage: true,
+          ),
+      ],
+    );
   }
 }
 
---
+++ lib/ui/widgets/packed_images_view.dart
@@ -0,0 +1,146 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2025 BitCodersNN
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class PackedImagesView extends StatelessWidget {
+  final List<Widget> children;
+  final FutureOr<void> Function(int index)? onChildTap;
+
+  const PackedImagesView({
+    super.key,
+    required this.children,
+    this.onChildTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return switch (children.length) {
+      0 => Container(),
+      1 => GestureDetector(
+          onTap: () async => await onChildTap?.call(0),
+          child: children[0],
+        ),
+      2 => Row(
+          children: children.indexed
+              .map(
+                (e) => _buildGridItem(e.$1, e.$2),
+              )
+              .toList(),
+        ),
+      3 || 4 => Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: GestureDetector(
+                onTap: () async => await onChildTap?.call(0),
+                child: FittedBox(
+                  fit: BoxFit.cover,
+                  child: children.first,
+                ),
+              ),
+            ),
+            ClipRect(
+              child: SizedBox(
+                height: 110.0,
+                child: Row(
+                  children: children.indexed
+                      .skip(1)
+                      .map((e) => _buildGridItem(e.$1, e.$2))
+                      .toList(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      _ => Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: GestureDetector(
+                onTap: () async => await onChildTap?.call(0),
+                child: FittedBox(
+                  fit: BoxFit.cover,
+                  child: children.first,
+                ),
+              ),
+            ),
+            ClipRect(
+              child: SizedBox(
+                height: 110.0,
+                child: Row(
+                  children: [
+                    ...children.indexed
+                        .skip(1)
+                        .take(2)
+                        .map((e) => _buildGridItem(e.$1, e.$2)),
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.all(1.0),
+                        child: GestureDetector(
+                          onTap: () async => await onChildTap?.call(3),
+                          child: Stack(
+                            children: [
+                              Positioned.fill(
+                                child: ColorFiltered(
+                                  colorFilter: ColorFilter.mode(
+                                    Colors.black.withAlpha(127),
+                                    BlendMode.srcATop,
+                                  ),
+                                  child: ClipRect(
+                                    child: FittedBox(
+                                      fit: BoxFit.cover,
+                                      child: children[3],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  '+${children.length - 3}',
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                    shadows: [
+                                      Shadow(
+                                        color: Colors.black,
+                                        blurRadius: 6,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+    };
+  }
+
+  Expanded _buildGridItem(int index, Widget child) {
+    return Expanded(
+      flex: 1,
+      child: Padding(
+        padding: const EdgeInsets.all(1.0),
+        child: GestureDetector(
+          onTap: () async => await onChildTap?.call(index),
+          child: FittedBox(fit: BoxFit.cover, child: child),
+        ),
+      ),
+    );
+  }
+}
