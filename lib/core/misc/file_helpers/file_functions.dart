344c6454ec585d11d3ae351637b2af0966073d8e
Yurin Andrey <andreyyurin@icloud.com>
2025-05-20 21:54:15 +0300

    Merge branch 'minorchange/0.5.0-pre-release-fixes' of https://github.com/BitCodersNN/unn-mobile into minorchange/0.5.0-pre-release-fixes

--- lib/core/misc/file_helpers/file_functions.dart
+++ lib/core/misc/file_helpers/file_functions.dart
@@ -3,6 +3,7 @@
 
 import 'dart:io';
 
+import 'package:content_resolver/content_resolver.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
@@ -34,17 +35,35 @@ Future<Iterable<String>?> openUploadFilePicker(bool gallery) async {
     const pickerEvents = EventChannel('ru.unn.unn_mobile/file_events');
     final pickerStream = pickerEvents.receiveBroadcastStream();
     final locations = await pickerStream.first as List<Object?>?;
-    return locations?.map(
-      (l) => l as String,
-    );
+    final uriStrings = locations?.map(
+          (l) => l as String,
+        ) ??
+        [];
+    return await resolveAndroidContentUris(uriStrings);
   } else {
     final FilePickerResult? result = await FilePicker.platform.pickFiles(
-      type: gallery ? FileType.image : FileType.any,
+      type: gallery ? FileType.media : FileType.any,
       allowMultiple: true,
     );
     if (result == null) return [];
     return result.paths.where((path) => path != null).cast<String>().toList();
   }
+}
+
+Future<List<String>> resolveAndroidContentUris(
+  Iterable<String> uriStrings,
+) async {
+  final cacheDir = await getApplicationCacheDirectory();
+  final tempDir = await cacheDir.createTemp();
+  final List<String> paths = [];
+  for (final uri in uriStrings) {
+    final r = await ContentResolver.resolveContentMetadata(uri);
+    final name = p.basename(r.fileName ?? 'null.txt');
+    final filePath = '${tempDir.path}/$name';
+    await ContentResolver.resolveContentToFile(uri, filePath);
+    paths.add(filePath);
+  }
+  return paths;
 }
 
 Future<void> viewFile(String uri, String mimeType) async {
