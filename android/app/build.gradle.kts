435e0836e97481cd4b744bd14db3d27a2157eae1
Mikhail Kriseev <mish.kryseev@gmail.com>
2025-09-15 23:15:05 +0300

    Обновил конфиги сборки андроида для Firebase. Пофиксил билд apk (надеюсь) (#551)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,10 @@ import java.io.FileInputStream
 
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
