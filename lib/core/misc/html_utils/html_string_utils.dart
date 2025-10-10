b2971afaf8d0126b11597c899ee1db1a9733100b
UNN MOBILE runner <unnmobile@mail.ru>
2025-10-10 23:07:48 +0000

    Add license headers to files

--- lib/core/misc/html_utils/html_string_utils.dart
+++ lib/core/misc/html_utils/html_string_utils.dart
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2025 BitCodersNN
+
 String removeHtmlComments(String html) {
   return html.replaceAllMapped(
     RegExp(r'<!--.*?-->', multiLine: true, dotAll: true),
