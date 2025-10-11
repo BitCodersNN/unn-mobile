c89e4d968718048cdae18d815a411ec24bc14e03
Yurin Andrey <100288192+Namxobick@users.noreply.github.com>
2025-10-11 12:24:35 +0300

    Оптимизация получения авторов приложения (#566)

---
+++ lib/core/providers/interfaces/about/last_commit_sha_authors_provider.dart
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2025 BitCodersNN
+
+import 'package:unn_mobile/core/providers/interfaces/common/last_commit_sha_provider.dart';
+
+abstract interface class LastCommitShaAuthorsProvider
+    implements LastCommitShaProvider {}
