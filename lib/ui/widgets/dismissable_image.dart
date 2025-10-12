370dfd3554c288efe31781aaa7240175c6d865a4
MadKris <mkris.yt@gmail.com>
2025-10-12 14:02:12 +0300

    review fix 2

---
+++ lib/ui/widgets/dismissable_image.dart
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2025 BitCodersNN
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/material.dart';
+
+class DismissibleImage extends StatelessWidget {
+  const DismissibleImage({
+    super.key,
+    required this.image,
+  });
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return ExtendedImage(
+      enableLoadState: true,
+      mode: ExtendedImageMode.gesture,
+      enableSlideOutPage: true,
+      image: CachedNetworkImageProvider(
+        image,
+      ),
+      initGestureConfigHandler: (state) => GestureConfig(
+        minScale: 0.9,
+        animationMinScale: 0.7,
+        maxScale: 3.0,
+        animationMaxScale: 3.5,
+        speed: 1.0,
+        inertialSpeed: 100.0,
+        initialScale: 1.0,
+        inPageView: false,
+        initialAlignment: InitialAlignment.center,
+      ),
+    );
+  }
+}
