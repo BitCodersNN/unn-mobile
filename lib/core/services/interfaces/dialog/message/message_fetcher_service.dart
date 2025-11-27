74001a44a6a1b5b1635ff45768ee6789b789aac6
Yurin Andrey <andreyyurin@icloud.com>
2025-11-27 19:40:22 +0300

    Реализовал

--- lib/core/services/interfaces/dialog/message/message_fetcher_service.dart
+++ lib/core/services/interfaces/dialog/message/message_fetcher_service.dart
@@ -4,6 +4,7 @@
 import 'package:unn_mobile/core/misc/objects_with_pagination.dart';
 import 'package:unn_mobile/core/models/dialog/message/message.dart';
 
+
 abstract interface class MessageFetcherService {
   /// Получает пагинированный список сообщений из чата.
   ///
@@ -20,9 +21,26 @@ abstract interface class MessageFetcherService {
   ///
   /// В случае ошибки:
   ///   - Возвращает `null`
-  Future<PaginatedResult<Message>?> fetch({
+  Future<PaginatedResult<Message>?> fetchByChatId({
     required int chatId,
     int limit,
     int? lastMessageId,
+  });
+
+  /// Получает пагинированный список сообщений из чата.
+  ///
+  /// Параметры:
+  ///   - [dialogId] - идентификатор чата, из которого запрашиваются сообщения (обязательный).
+  ///   - [limit] - максимальное количество сообщений в результате (по умолчанию 25)
+  ///
+  /// Возвращает [PaginatedResult] с:
+  ///   - Списком сообщений ([items])
+  ///   - Флагами наличия предыдущей/следующей страниц ([hasPreviousPage], [hasNextPage])
+  ///
+  /// В случае ошибки:
+  ///   - Возвращает `null`
+  Future<PaginatedResult<Message>?> fetchByDialogId({
+    required String dialogId,
+    int limit = 25,
   });
 }
