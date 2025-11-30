a2d46caa11b28ce8841f842d7a8c19ebf78f7ff6
Yurin Andrey <andreyyurin@icloud.com>
2025-11-30 22:53:30 +0300

    Добавил поле userId

--- lib/core/models/about/author.dart
+++ lib/core/models/about/author.dart
@@ -7,12 +7,14 @@ import 'package:unn_mobile/core/constants/api/protocol_type.dart';
 import 'package:unn_mobile/core/misc/json/json_utils.dart';
 
 class _AuthorJsonKeys {
+  static const String userId = 'user_id';
   static const String fullname = 'fullname';
   static const String educationGroup = 'education_group';
   static const String avatar = 'avatar';
 }
 
 class Author {
+  final int? userId;
   final String fullname;
   final String? educationGroup;
   final String avatar;
@@ -23,16 +25,19 @@ class Author {
   Author({
     required this.fullname,
     required this.avatar,
+    this.userId,
     this.educationGroup,
   });
 
   factory Author.fromJson(JsonMap json) => Author(
+        userId: json[_AuthorJsonKeys.userId] as int?,
         fullname: json[_AuthorJsonKeys.fullname]! as String,
         educationGroup: json[_AuthorJsonKeys.educationGroup] as String?,
         avatar: json[_AuthorJsonKeys.avatar]! as String,
       );
 
   JsonMap toJson() => {
+        _AuthorJsonKeys.userId: userId,
         _AuthorJsonKeys.fullname: fullname,
         _AuthorJsonKeys.educationGroup: educationGroup,
         _AuthorJsonKeys.avatar: avatar,
