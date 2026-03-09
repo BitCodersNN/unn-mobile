b1979c29f4416d8ca1b522cebcabc38fe72f98ff
UNN MOBILE runner <unnmobile@mail.ru>
2026-03-09 21:08:26 +0000

    Add license headers to files

--- lib/core/constants/api/feed_data_keys_and_values.dart
+++ lib/core/constants/api/feed_data_keys_and_values.dart
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026 BitCodersNN
+
 import 'package:unn_mobile/core/constants/string_keys/session_identifier_keys.dart';
 
 class DataKeys {
--- lib/core/models/feed/post_destination.dart
+++ lib/core/models/feed/post_destination.dart
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026 BitCodersNN
+
 class PostDestination {
   final String type;
   final int id;
