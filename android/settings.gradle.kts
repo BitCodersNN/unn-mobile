2643bdcb50f04ac5dce448229c785cf17dd54d80
Mikhail Kriseev <mish.kryseev@gmail.com>
2026-08-28 16:39:10 +0300

    fix build (hopefully)

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -20,7 +20,7 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "9.1.0" apply false
     // START: FlutterFire Configuration
-    id("com.google.gms.google-services") version("4.3.15") apply false
+    id("com.google.gms.google-services") version("4.4.1") apply false
     id("com.google.firebase.crashlytics") version "3.0.8" apply false
     // END: FlutterFire Configuration
     id("org.jetbrains.kotlin.android") version "2.4.0" apply false
