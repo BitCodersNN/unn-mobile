3d7b987cff48cd381c149cc474b9f6c20495e365
Mikhail Kriseev <mish.kryseev@gmail.com>
2026-08-28 15:19:23 +0300

    Add Firebase Crashlytics plugin to build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
     id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
     // END: FlutterFire Configuration
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
