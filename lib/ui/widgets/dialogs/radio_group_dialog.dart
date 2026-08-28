1167c6f5e4ee50f5ace3d5af2f64c811bca897a3
Mikhail Kriseev <mish.kryseev@gmail.com>
2026-08-29 01:00:29 +0300

    Replace TextButton with AdaptiveDialogAction

--- lib/ui/widgets/dialogs/radio_group_dialog.dart
+++ lib/ui/widgets/dialogs/radio_group_dialog.dart
@@ -93,21 +93,14 @@ class _RadioGroupDialogState extends State<RadioGroupDialog> {
         ),
       ),
       actions: [
-        TextButton(
+        AdaptiveDialogAction(
           onPressed: () => Navigator.pop(context),
           child: widget.cancelButtonChild,
         ),
-        TextButton(
+        AdaptiveDialogAction(
           onPressed: () => Navigator.pop(context, selectedValue),
           child: widget.okButtonChild,
-AdaptiveDialogAction(
-onPressed: () => Navigator.pop(context),
-child: widget.cancelButtonChild,
-),
-AdaptiveDialogAction(
-onPressed: () => Navigator.pop(context, selectedValue),
-child: widget.okButtonChild,
-),
+        )
       ],
     );
   }
