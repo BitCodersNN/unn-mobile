6f287174345a3a40eafa7111875f902e959b2539
Mikhail Kriseev <mish.kryseev@gmail.com>
2026-08-28 15:18:15 +0300

    Add Firebase Crashlytics plugin to settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -21,6 +21,7 @@ plugins {
     id("com.android.application") version "9.1.0" apply false
     // START: FlutterFire Configuration
     id("com.google.gms.google-services") version("4.3.15") apply false
+    id("com.google.firebase.crashlytics") version "3.0.8" apply false
     // END: FlutterFire Configuration
     id("org.jetbrains.kotlin.android") version "2.4.0" apply false
 }
